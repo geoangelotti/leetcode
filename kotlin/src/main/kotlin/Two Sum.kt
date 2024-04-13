61433358acf6ac28bb3b8355eb2fc8aa94bcdc78
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-13 18:36:24 +0200

    a bit faster solution

--- kotlin/src/main/kotlin/Two Sum.kt
+++ kotlin/src/main/kotlin/Two Sum.kt
@@ -1,15 +1,12 @@
 package o.mg
 
 fun twoSum(nums: IntArray, target: Int): IntArray {
-    val res = IntArray(2)
-    for (i in nums.withIndex()) {
-        for (y in nums.withIndex()) {
-            if ((i.value + y.value == target) && (i.index != y.index)) {
-                res[0] = i.index
-                res[1] = y.index
-                return res
+    for(i in nums.indices) {
+        for (j in i+1..<nums.size) {
+            if (nums[i] + nums[j] == target) {
+                return intArrayOf(i, j)
             }
         }
     }
-    return res
+    return intArrayOf()
 }
