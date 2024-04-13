f89efa51b08f5e2351044eb4addc04a6f64ca7d5
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-13 16:55:23 +0200

    Two Sum solution
    O(n^2) complexity

---
+++ kotlin/src/main/kotlin/Two Sum.kt
@@ -0,0 +1,18 @@
+package o.mg
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val res = IntArray(2)
+    for (i in nums.withIndex()) {
+        if (i.value > target) {
+            continue
+        }
+        for (y in nums.withIndex()) {
+            if ((i.value + y.value == target) && (i.index != y.index)) {
+                res[0] = i.index
+                res[1] = y.index
+                return res
+            }
+        }
+    }
+    return res
+}
