947c14719fbcf1b294f9ed59d8d55e149c6da09d
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-17 20:51:15 +0200

    Group Anagram more kotlin way

--- kotlin/src/main/kotlin/Group Anagrams.kt
+++ kotlin/src/main/kotlin/Group Anagrams.kt
@@ -1,15 +1,7 @@
 package o.mg
 
-fun groupAnagrams(strs: Array<String>): List<List<String>> {
-    val res = mutableMapOf<Map<Char, Int>,MutableList<String>>()
-    for (s in strs) {
-        val entry = s.groupingBy { it }.eachCount()
-        val lookup = res[entry]
-        if (lookup == null) {
-            res[entry] = mutableListOf(s)
-        } else {
-            lookup.add(s)
-        }
-    }
-    return res.values.toList()
-}
+fun groupAnagrams(strs: Array<String>): List<List<String>> =
+    strs.groupBy { str -> str.groupingBy { it }.eachCount() }
+        .values
+        .toList()
+
