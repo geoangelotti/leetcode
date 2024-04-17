dd04c74d580571953d67ce6427a662503aa031f4
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-17 22:02:45 +0200

    Product of Array Except Self test

---
+++ kotlin/src/test/kotlin/Product of Array Except Self Test.kt
@@ -0,0 +1,17 @@
+import o.mg.productExceptSelf
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class `Product of Array Except Self Test` {
+    @Test
+    fun case1() {
+        val result = productExceptSelf(intArrayOf(1, 2, 3, 4))
+        assertEquals(result.toList(), arrayListOf(24, 12, 8, 6))
+    }
+
+    @Test
+    fun case2() {
+        val result = productExceptSelf(intArrayOf(-1, 1, 0, -3, 3))
+        assertEquals(result.toList(), arrayListOf(0, 0, 9, 0, 0))
+    }
+}
