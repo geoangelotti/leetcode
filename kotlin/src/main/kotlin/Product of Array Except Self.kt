4dfc6b57ea728603e95d932a68c73e2794bd3901
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-17 22:08:29 +0200

    Product of Array Except Self better solution
    O(n)
    calculate a left and right product and then combine

--- kotlin/src/main/kotlin/Product of Array Except Self.kt
+++ kotlin/src/main/kotlin/Product of Array Except Self.kt
@@ -1,15 +1,18 @@
 package o.mg
 
 fun productExceptSelf(nums: IntArray): IntArray {
-    val res = IntArray(nums.size)
-    for (i in nums.indices) {
-        var prod = 1
-        for (j in nums.indices) {
-            if (i!=j) {
-                prod *= nums[j]
-            }
-        }
-        res[i] = prod
+    val n = nums.size
+    val left = IntArray(n) { 1 }
+    val right = IntArray(n) { 1 }
+    for (i in 1 until n) {
+        left[i] = left[i - 1] * nums[i - 1]
+    }
+    for (i in n - 2 downTo 0) {
+        right[i] = right[i + 1] * nums[i + 1]
+    }
+    val res = IntArray(n) { 0 }
+    for (i in 0 until n) {
+        res[i] = left[i] * right[i]
     }
     return res
 }
