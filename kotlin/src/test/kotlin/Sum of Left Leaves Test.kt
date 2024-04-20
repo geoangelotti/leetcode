28ce0816af53f5010e6019adbe427d4d432f9dab
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-20 09:32:36 +0200

    name leaves in the tests

--- kotlin/src/test/kotlin/Sum of Left Leaves Test.kt
+++ kotlin/src/test/kotlin/Sum of Left Leaves Test.kt
@@ -6,14 +6,14 @@ import kotlin.test.assertEquals
 class `Sum of Left Leaves Test` {
     @Test
     fun case1() {
-        val n7 = TreeNode(7)
-        val n15 = TreeNode(15)
+        val l7 = TreeNode(7)
+        val l15 = TreeNode(15)
         val n20 = TreeNode(20)
-        n20.left = n15
-        n20.right = n7
-        val n9 = TreeNode(9)
+        n20.left = l15
+        n20.right = l7
+        val l9 = TreeNode(9)
         val n3 = TreeNode(3)
-        n3.left = n9
+        n3.left = l9
         n3.right = n20
         val result = sumOfLeftLeaves(n3)
         assertEquals(result, 24)
@@ -25,4 +25,6 @@ class `Sum of Left Leaves Test` {
         val result = sumOfLeftLeaves(root)
         assertEquals(result, 0)
     }
+
+
 }
