a48a0c3b6696dbab58edb9b8ec53c604e5c8c055
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-22 21:04:35 +0200

    solution with joinToString

--- kotlin/src/main/kotlin/Check If Two String Arrays are Equivalent.kt
+++ kotlin/src/main/kotlin/Check If Two String Arrays are Equivalent.kt
@@ -1,6 +1,5 @@
 package o.mg
 
 fun arrayStringsAreEqual(word1: Array<String>, word2: Array<String>): Boolean {
-    return word1.joinTo(StringBuilder(), separator = "").toString() == word2.joinTo(StringBuilder(), separator = "")
-        .toString()
+    return word1.joinToString(separator = "") == word2.joinToString(separator = "")
 }
