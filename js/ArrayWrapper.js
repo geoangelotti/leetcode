c965e05af152655517726651921773a899fc53f5
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-24 19:46:38 +0200

    ArrayWrapper solution

--- js/ArrayWrapper.js
+++ js/ArrayWrapper.js
@@ -2,16 +2,22 @@
  * @param { number[] } nums
  * @return { void}
  */
-var ArrayWrapper = function (nums) {};
+var ArrayWrapper = function (nums) {
+	this.nums = nums;
+};
 
 /**
  * @return {number}
  */
-ArrayWrapper.prototype.valueOf = function () {};
+ArrayWrapper.prototype.valueOf = function () {
+	return this.nums.reduce((acc, c) => acc + Number(c), 0);
+};
 
 /**
  * @return {string}
  */
-ArrayWrapper.prototype.toString = function () {};
+ArrayWrapper.prototype.toString = function () {
+	return "[" + this.nums.join(",") + "]";
+};
 
 module.exports = ArrayWrapper;
