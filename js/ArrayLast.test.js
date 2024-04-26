304928801098d33cbdcb64ab4491716c336a7201
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-26 18:40:47 +0200

    fix array creation

--- js/ArrayLast.test.js
+++ js/ArrayLast.test.js
@@ -1,11 +1,11 @@
 const Array = require("./ArrayLast");
 
 test("case 1", () => {
-	const nums = new Array([null, {}, 3])
+	const nums = new Array(null, {}, 3)
 	expect(nums.last()).toBe(3);
 });
 
 test("case 2", () => {
-	const nums = new Array([])
+	const nums = new Array()
 	expect(nums.last()).toBe(-1);
 });
