33722f1a060003dde508ec2f69aeb03521036920
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-26 21:25:00 +0200

    array filter test

---
+++ js/ArrayFilter.test.js
@@ -0,0 +1,19 @@
+const filter = require("./ArrayFilter");
+
+test("case 1", () => {
+	const arr = [0,10,20,30];
+	function greaterThan10(n) { return n > 10; };
+	expect(filter(arr, greaterThan10)).toStrictEqual([20, 30]);
+});
+
+test("case 2", () => {
+	const arr = [1,2,3];
+	function firstIndex(n, i) { return i === 0; };
+	expect(filter(arr, firstIndex)).toStrictEqual([1]);
+});
+
+test("case 3", () => {
+	const arr = [-2,-1,0,1,2];
+	function plusOne(n) { return n + 1 };
+	expect(filter(arr, greaterThan10)).toStrictEqual([20, 30]);
+});
