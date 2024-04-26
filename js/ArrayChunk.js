ff9e5567b6d8f658abe3001bc8e2630e5e622de3
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-26 19:54:07 +0200

    Array chunk solution

--- js/ArrayChunk.js
+++ js/ArrayChunk.js
@@ -3,7 +3,14 @@
  * @param {number} size
  * @return {Array}
  */
-var chunk = function(arr, size) {    
+var chunk = function(arr, size) {
+	let chunkedArray = [];
+	let index = 0;
+	while (index < arr.length) {
+		chunkedArray.push(arr.slice(index, index + size));
+		index += size;
+	}
+	return chunkedArray;
 };
 
 module.exports = chunk;
