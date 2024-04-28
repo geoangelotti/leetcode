fcc0c636412a89ef066cb30973803aa04f7bafe1
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-28 14:49:27 +0200

    Fibonacci generator solution

--- js/src/FibonacciGenerator.js
+++ js/src/FibonacciGenerator.js
@@ -2,7 +2,14 @@
  * @return {Generator<number>}
  */
 var fibGenerator = function*() {
-    
+    let current = 0; 
+	let next = 1;
+	while (true) {
+		yield current; 
+		let temp = current + next;
+		current = next;
+		next = temp;
+	}
 };
 
 module.exports = fibGenerator;
