c1470c4e537f0b6e952436001e6eaa926f103084
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-28 11:28:22 +0200

    Counter 2 solution

--- js/src/Counter2.js
+++ js/src/Counter2.js
@@ -3,7 +3,12 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
-    
+	let c = init;
+	return {
+		increment: () => { return ++c },
+		reset: () => { return c = init; },
+		decrement: () => { return --c },
+	};
 };
 
 module.exports = createCounter;
