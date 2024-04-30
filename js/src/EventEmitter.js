f29cdbf6ebacb268bbb5783dfe07105e5277b396
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-01 00:05:33 +0200

    Event emitter solution

--- js/src/EventEmitter.js
+++ js/src/EventEmitter.js
@@ -1,4 +1,7 @@
 class EventEmitter {
+	constructor() {
+		this.events = new Map();
+	}
     
     /**
      * @param {string} eventName
@@ -6,12 +9,18 @@ class EventEmitter {
      * @return {Object}
      */
     subscribe(eventName, callback) {
-        
-        return {
-            unsubscribe: () => {
-                
-            }
-        };
+        if (!this.events.has(eventName)) {
+			this.events.set(eventName, []);
+	  	}
+		const subscribers = this.events.get(eventName);
+  		const unsubscribe = () => {
+			const index = subscribers.indexOf(callback);
+			if (index !== -1) {
+			subscribers.splice(index, 1);
+			}
+	  	};
+	  	subscribers.push(callback);
+ 	  	return { unsubscribe };
     }
     
     /**
@@ -20,8 +29,16 @@ class EventEmitter {
      * @return {Array}
      */
     emit(eventName, args = []) {
-        
-    }
+		const subscribers = this.events.get(eventName);
+		const results = [];
+		if (!subscribers) {
+		  return results;
+		}
+		for (callback of subscribers) {
+			results.push(callback(...args));
+		};
+		return results;
+	  }
 }
 
 module.exports = EventEmitter;
