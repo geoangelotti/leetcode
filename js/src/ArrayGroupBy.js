c6f345ac7c05b00ae6575ba1f794264fa8d79cc4
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-01 00:19:36 +0200

    Array group by solution

--- js/src/ArrayGroupBy.js
+++ js/src/ArrayGroupBy.js
@@ -3,7 +3,17 @@
  * @return {Object}
  */
 Array.prototype.groupBy = function(fn) {
-    
+    let result = {};
+    this.forEach( (i) =>{
+        const r =  fn(i);
+        if (result[r]) {
+            result[r].push(i);
+        }
+        else {
+            result[r] = [i];
+        }
+    })
+    return result;
 };
 
 module.exports = Array;
