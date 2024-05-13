efc5613b4748dedf82acf539e63692f72f454949
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-13 10:19:58 +0300

    Check if Instance of Class solution

--- js/src/CheckIfInstanceOf.js
+++ js/src/CheckIfInstanceOf.js
@@ -4,7 +4,14 @@
  * @return {boolean}
  */
 var checkIfInstanceOf = function(obj, classFunction) {
-    
+	if (obj === null || obj === undefined || typeof classFunction !== "function") {
+		return false;
+	}
+	let inputObj = obj;
+	if (typeof obj !== "object") {
+		inputObj = Object(obj);
+	}
+	return inputObj instanceof classFunction;
 };
 
 module.exports = checkIfInstanceOf;
