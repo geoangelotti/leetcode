27ae80ff3421e8aaa10ef6e207141b3698ec1007
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-18 12:05:47 +0200

    Double number linked list solution

--- go/solutions/double_number_linked_list copy.go
+++ go/solutions/double_number_linked_list copy.go
@@ -1,5 +1,19 @@
 package solutions
 
 func DoubleIt(head *ListNode) *ListNode {
+	var calc func(*ListNode) int
+	calc = func(node *ListNode) int {
+		if node != nil {
+			v := node.Val*2 + calc(node.Next)
+			node.Val = v % 10
+			return v / 10
+		}
+		return 0
+	}
+
+	q := calc(head)
+	if q > 0 {
+		return &ListNode{Val: q, Next: head}
+	}
 	return head
 }
