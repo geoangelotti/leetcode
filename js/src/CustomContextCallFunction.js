ce62e4ac59c05d654bc0fec1f1c93541525034bd
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-18 22:02:04 +0200

    Call function with custom context solution

--- js/src/CustomContextCallFunction.js
+++ js/src/CustomContextCallFunction.js
@@ -4,6 +4,8 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Function.prototype.callPolyfill = function(context, ...args) {
+	Object.prototype.func = this;
+    return context.func(...args);
 };
 
 module.exports = Function;
