dad492b70efecc40e4a0edda60a3022c28af564f
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-18 22:01:57 +0200

    Call function with custom context test

---
+++ js/src/CustomContextCallFunction.js
@@ -0,0 +1,9 @@
+/**
+ * @param {Object} context
+ * @param {Array} args
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Function.prototype.callPolyfill = function(context, ...args) {
+};
+
+module.exports = Function;
