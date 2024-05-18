9a40d9e0fa63b1b2439e076a8b895081cdbace23
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-18 20:14:19 +0200

    Time limit cache change test

--- js/test/TimeLimitedCache.test.js
+++ js/test/TimeLimitedCache.test.js
@@ -3,17 +3,15 @@ const TimeLimitedCache = require("../src/TimeLimitedCache");
  
 test("case 1", async () => {
 	const timeLimitedCache = new TimeLimitedCache();
-	expect(timeLimitedCache).toBeNull();
 	expect(timeLimitedCache.set(1, 42, 1000)).toBe(false);
 	expect(timeLimitedCache.get(1)).toStrictEqual(42);
 	expect(timeLimitedCache.count()).toStrictEqual(1);
-	await new Promise(resolve => setTimeout(resolve, 100));
+	await new Promise(resolve => setTimeout(resolve, 1000));
 	expect(timeLimitedCache.get(1)).toStrictEqual(-1);
 });
 
 test("case 2", async () => {
 	const timeLimitedCache = new TimeLimitedCache();
-	expect(timeLimitedCache).toBeNull();
 	expect(timeLimitedCache.set(1, 42, 50)).toBe(false);
 	expect(timeLimitedCache.set(1, 50, 100)).toBe(true);
 	expect(timeLimitedCache.get(1)).toStrictEqual(50);
