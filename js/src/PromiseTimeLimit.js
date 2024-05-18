dbb6b679a5049625d27a3e42103def2f5d1b3586
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-18 21:49:31 +0200

    Promise with time limit test

---
+++ js/src/PromiseTimeLimit.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+    };
+};
+
+module.exports = timeLimit;
