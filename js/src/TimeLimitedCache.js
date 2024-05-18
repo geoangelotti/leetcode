78d9be357f4a75793a6b1391895269c080650a34
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-18 20:14:25 +0200

    Time limit cache solution

--- js/src/TimeLimitedCache.js
+++ js/src/TimeLimitedCache.js
@@ -1,4 +1,6 @@
-var TimeLimitedCache = function() {  
+var TimeLimitedCache = function() {
+    this.Cache = new Map();
+	return null
 };
 
 /** 
@@ -7,7 +9,19 @@ var TimeLimitedCache = function() {
  * @param {number} duration time until expiration in ms
  * @return {boolean} if un-expired key already existed
  */
-TimeLimitedCache.prototype.set = function(key, value, duration) {  
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    let res = false;
+    if(this.Cache.has(key)) {
+        const ref = this.Cache.get(key).ref;
+        clearTimeout(ref);
+        res = true;
+    }
+    const ref = setTimeout(() => {this.Cache.delete(key);}, duration);
+    this.Cache.set(key, {
+        value: value,
+        ref: ref
+    });
+    return res;
 };
 
 /** 
@@ -15,12 +29,17 @@ TimeLimitedCache.prototype.set = function(key, value, duration) {
  * @return {number} value associated with key
  */
 TimeLimitedCache.prototype.get = function(key) {
+    if(this.Cache.has(key)) {
+       return this.Cache.get(key).value;  
+    }
+    return -1;
 };
 
 /** 
  * @return {number} count of non-expired keys
  */
 TimeLimitedCache.prototype.count = function() {
+    return this.Cache.size;
 };
 
 module.exports = TimeLimitedCache;
