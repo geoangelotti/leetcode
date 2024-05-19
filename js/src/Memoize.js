d0dd3f5b67757f35ff75c107245cf87096fe7b9d
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-19 13:34:37 +0200

    Memoize solution

--- js/src/Memoize.js
+++ js/src/Memoize.js
@@ -3,9 +3,15 @@
  * @return {Function}
  */
 function memoize(fn) {
-    
+	const c = {};
     return function(...args) {
-        
+        const key = JSON.stringify(args);
+        if (key in c) {
+            return c[key];
+        }
+        const result = fn.apply(this, args);
+        c[key] = result;    
+        return result;
     }
 }
 
