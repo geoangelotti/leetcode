eec36a76b637c7c9b19b88a79b667e818de6c8fc
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-19 13:05:45 +0200

    merged increment and array lookup

--- kotlin/src/main/kotlin/Median of Two Sorted Arrays.kt
+++ kotlin/src/main/kotlin/Median of Two Sorted Arrays.kt
@@ -6,20 +6,16 @@ fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
     var j = 0
     while (i < nums1.size && j < nums2.size) {
         if (nums1[i] < nums2[j]) {
-            merged.add(nums1[i])
-            i++
+            merged.add(nums1[i++])
         } else {
-            merged.add(nums2[j])
-            j++
+            merged.add(nums2[j++])
         }
     }
     while (i < nums1.size) {
-        merged.add(nums1[i])
-        i++
+        merged.add(nums1[i++])
     }
     while (j < nums2.size) {
-        merged.add(nums2[j])
-        j++
+        merged.add(nums2[j++])
     }
     return if (merged.size % 2 == 0) {
         (merged[merged.size / 2].toDouble() + merged[merged.size / 2 - 1].toDouble()) / 2
