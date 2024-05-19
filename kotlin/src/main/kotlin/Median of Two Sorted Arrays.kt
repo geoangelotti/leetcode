c82a12aef8b272da9512e5f4e0f251841b12d11b
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-19 12:56:36 +0200

    Median of Two Sorted Arrays solution

--- kotlin/src/main/kotlin/Median of Two Sorted Arrays.kt
+++ kotlin/src/main/kotlin/Median of Two Sorted Arrays.kt
@@ -1,5 +1,28 @@
 package o.mg
 
 fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
-    TODO()
+    val merged = mutableListOf<Int>()
+    var i = 0
+    var j = 0
+    while (i < nums1.size && j < nums2.size) {
+        if (nums1[i] < nums2[j]) {
+            merged.add(nums1[i])
+            i++
+        } else {
+            merged.add(nums2[j])
+            j++
+        }
+    }
+    while (i < nums1.size) {
+        merged.add(nums1[i])
+        i++
+    }
+    while (j < nums2.size) {
+        merged.add(nums2[j])
+        j++
+    }
+    if (merged.size % 2 == 0) {
+        return (merged[merged.size / 2].toDouble() + merged[merged.size / 2 - 1].toDouble()) / 2
+    }
+    return merged[merged.size / 2].toDouble()
 }
