6cc1ae5fd5b4dde1d651b531456b49be19e39f77
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-20 23:25:26 +0200

    Debounce skeleton

---
+++ js/src/Debounce.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    
+    return function(...args) {
+        
+    }
+};
+
+module.exports = debounce;
