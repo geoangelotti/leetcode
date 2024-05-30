71bcab426040cafa47b4791b8479233d1db7f453
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-05-30 12:08:02 +0200

    Student Attendance Record I solution
    5% percentile :|

--- kotlin/src/main/kotlin/Student Attendance Record I.kt
+++ kotlin/src/main/kotlin/Student Attendance Record I.kt
@@ -1,5 +1,7 @@
 package o.mg
 
 fun checkRecord(s: String): Boolean {
-    TODO()
+    val absent = s.count { it == 'A' } < 2
+    val late = s.windowed(3).count { it == "LLL" } < 1
+    return absent && late
 }
