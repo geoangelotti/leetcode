ddfe8ba70af505ac4a51e79377f4a329bab22533
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-09 19:13:55 +0200

    Richest Customer Wealth solution

--- scala/src/main/scala/RichestCustomerWealth.scala
+++ scala/src/main/scala/RichestCustomerWealth.scala
@@ -1,8 +1,4 @@
 
 def maximumWealth(accounts: Array[Array[Int]]): Int = {
-  throw new NotImplementedError()
-}
-
-def main(): Unit = {
-  println(maximumWealth(Array(Array(1))))
+  accounts.map(_.sum).max
 }
