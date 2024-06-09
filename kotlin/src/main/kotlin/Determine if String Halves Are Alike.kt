87f3bf646fc3f9dd10e363a85c2247cd670cbe0a
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-09 14:18:20 +0200

    take vowels to their own file

--- kotlin/src/main/kotlin/Determine if String Halves Are Alike.kt
+++ kotlin/src/main/kotlin/Determine if String Halves Are Alike.kt
@@ -1,7 +1,5 @@
 package o.mg
 
-val vowels = setOf('a', 'e', 'i', 'o', 'u')
-
 fun halvesAreAlike(s: String): Boolean {
     val s1 = s.substring(0, s.length / 2)
     val s2 = s.substring(s.length / 2)
