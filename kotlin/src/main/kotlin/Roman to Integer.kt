26300e8c352b1db8366560af0d871fdd92ebada2
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-11 19:54:01 +0200

    Roman to Integer solution

--- kotlin/src/main/kotlin/Roman to Integer.kt
+++ kotlin/src/main/kotlin/Roman to Integer.kt
@@ -1,5 +1,36 @@
 package o.mg
 
+val romanToInt = mapOf(
+    'I' to 1,
+    'V' to 5,
+    'X' to 10,
+    'L' to 50,
+    'C' to 100,
+    'D' to 500,
+    'M' to 1000,
+)
+
 fun romanToInt(s: String): Int {
-    TODO()
+    val t = s.mapNotNull { romanToInt[it] }
+    var i = 0
+    var j = 0
+    var acc = 0
+    while (i < t.size) {
+        j = i + 1
+        val current = t[i]
+        if (j > t.size - 1) {
+            acc += current
+            i += 1
+            continue
+        }
+        val next = t[j]
+        if (next > current) {
+            acc = acc + next - current
+            i += 2
+            continue
+        }
+        acc += current
+        i += 1
+    }
+    return acc
 }
