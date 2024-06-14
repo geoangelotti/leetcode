bf0cb1ebd542515775195bd2afc943e132e73742
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-14 20:00:37 +0200

    Valid Palindrome test

---
+++ kotlin/src/main/kotlin/Valid Palindrome.kt
@@ -0,0 +1,5 @@
+package o.mg
+
+fun isPalindrome(s: String): Boolean {
+    TODO()
+}
---
+++ kotlin/src/test/kotlin/Valid Palindrome Test.kt
@@ -0,0 +1,23 @@
+import o.mg.isPalindrome
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class `Valid Palindrome Test` {
+    @Test
+    fun case1() {
+        val result = isPalindrome("A man, a plan, a canal: Panama")
+        assertTrue { result }
+    }
+
+    @Test
+    fun case2() {
+        val result = isPalindrome("race a car")
+        assertTrue { result }
+    }
+
+    @Test
+    fun case3() {
+        val result = isPalindrome(" ")
+        assertTrue { result }
+    }
+}
