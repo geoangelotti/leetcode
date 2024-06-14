79674e6a3e968ff6c322f2787d6101b33f17f980
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-14 19:35:57 +0200

    Minimum Increment to Make Array Unique solution

--- kotlin/src/main/kotlin/Minimum Increment to Make Array Unique.kt
+++ kotlin/src/main/kotlin/Minimum Increment to Make Array Unique.kt
@@ -1,5 +1,13 @@
 package o.mg
 
 fun minIncrementForUnique(nums: IntArray): Int {
-    TODO()
+    nums.sort()
+    var steps = 0
+    for (i in nums.indices) {
+        if (nums[i] <= nums[i - 1]) {
+            steps += nums[i - 1] - nums[i] + 1
+            nums[i] = nums[i - 1] + 1
+        }
+    }
+    return steps
 }
