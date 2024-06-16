f12427e3ac21de780941b1f829b2117d0738c608
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-16 18:30:24 +0200

    Valid Palindrome solution
    compare lowercase chars

--- kotlin/src/main/kotlin/Valid Palindrome.kt
+++ kotlin/src/main/kotlin/Valid Palindrome.kt
@@ -3,7 +3,7 @@ package o.mg
 fun isPalindrome(s: String): Boolean {
     val clean = s.filter { it.isLetter() || it.isDigit() }
     for (i in 0..<(clean.length / 2)) {
-        if (clean[i] != clean[clean.length - 1 - i]) {
+        if (clean[i].lowercaseChar() != clean[clean.length - 1 - i].lowercaseChar()) {
             return false
         }
     }
