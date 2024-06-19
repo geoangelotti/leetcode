6757e4713b17de25fc2446a2cbc0c0c8b5563e56
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-19 19:39:30 +0200

    Linked List Cycle solution
    with fast and slow pointers

--- kotlin/src/main/kotlin/Linked List Cycle.kt
+++ kotlin/src/main/kotlin/Linked List Cycle.kt
@@ -1,15 +1,13 @@
 package o.mg
 
 fun hasCycle(head: ListNode?): Boolean {
-    val seen = mutableSetOf<ListNode>()
     if (head == null) return false
-    var next: ListNode? = head.next
-    while (next != null) {
-        if (seen.contains(next)) {
-            return true
-        }
-        seen.add(next)
-        next = next.next
+    var slow = head
+    var fast = head.next
+    while (fast?.next != null && slow != null) {
+        if (fast == slow) return true
+        slow = slow.next
+        fast = fast.next!!.next
     }
     return false
 }
