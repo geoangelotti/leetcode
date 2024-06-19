c85a76e11bcc7a429a7c948037d3481ba15f0c47
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-19 19:34:29 +0200

    Linked List Cycle solution
    with hash set

--- kotlin/src/main/kotlin/Linked List Cycle.kt
+++ kotlin/src/main/kotlin/Linked List Cycle.kt
@@ -1,5 +1,15 @@
 package o.mg
 
 fun hasCycle(head: ListNode?): Boolean {
-    TODO()
+    val seen = mutableSetOf<ListNode>()
+    if (head == null) return false
+    var next: ListNode? = head.next
+    while (next != null) {
+        if (seen.contains(next)) {
+            return true
+        }
+        seen.add(next)
+        next = next.next
+    }
+    return false
 }
