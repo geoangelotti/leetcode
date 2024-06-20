8d8427fd47520f80837908d17c60ac7764aea048
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-20 19:49:44 +0200

    Detect Squares solution

--- kotlin/src/main/kotlin/Detect Squares.kt
+++ kotlin/src/main/kotlin/Detect Squares.kt
@@ -1,10 +1,32 @@
 package o.mg
 
+import kotlin.math.abs
+
 class `Detect Squares` {
+    private val points = mutableMapOf<Point, Int>()
+
     fun add(point: IntArray) {
+        points[Point(point[0], point[1])] = points.getOrDefault(Point(point[0], point[1]), 0) + 1
     }
 
     fun count(point: IntArray): Int {
-        TODO()
+        var acc = 0
+        points.keys.forEach {
+            val xDiff = abs(it.x - point[0])
+            val yDiff = abs(it.y - point[1])
+            if (xDiff != 0 && xDiff == yDiff && points.contains(Point(point[0], it.y)) && points.contains(
+                    Point(
+                        it.x,
+                        point[1]
+                    )
+                )
+            ) {
+                acc += (points.getOrDefault(Point(point[0], it.y), 0) * points.getOrDefault(
+                    Point(it.x, point[1]),
+                    0
+                ) * points.getOrDefault(it, 0))
+            }
+        }
+        return acc
     }
 }
