616cf937bc24e4de849b844894da37423f4b3af5
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-22 13:49:24 +0200

    Double a Number Represented as a Linked List test
    cleaner first case using list constructor

--- kotlin/src/test/kotlin/Double a Number Represented as a Linked List Test.kt
+++ kotlin/src/test/kotlin/Double a Number Represented as a Linked List Test.kt
@@ -6,18 +6,9 @@ import kotlin.test.assertEquals
 class `Double a Number Represented as a Linked List Test` {
     @Test
     fun case1() {
-        val l1 = ListNode(1)
-        val l2 = ListNode(8)
-        l1.next = l2
-        val l3 = ListNode(9)
-        l2.next = l3
-        val result = doubleIt(l1)
-        val a1 = ListNode(3)
-        val a2 = ListNode(7)
-        a1.next = a2
-        val a3 = ListNode(8)
-        a2.next = a3
-        assertEquals(result, a1)
+        val head = ListNode(listOf(1,8,9))
+        val result = doubleIt(head)
+        assertEquals(ListNode(listOf(3,7,8)), result)
     }
 
     @Test
