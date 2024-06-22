c209ea101be9dddfa67eafdce3a6a6c432ff1669
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-22 13:18:57 +0200

    ListNode override equals method

--- kotlin/src/main/kotlin/ListNode.kt
+++ kotlin/src/main/kotlin/ListNode.kt
@@ -2,4 +2,15 @@ package o.mg
 
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is ListNode?) {
+            return false
+        }
+        if (other == null) return true
+        if ( this.`val` == other.`val`) {
+            return this.next == other.next
+        }
+        return false
+    }
 }
