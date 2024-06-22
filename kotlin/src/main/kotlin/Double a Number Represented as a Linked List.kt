28b752d23d4e6bfff8cd1aac1458c468f35dff3a
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-22 13:46:01 +0200

    Double a Number Represented as a Linked List solution

--- kotlin/src/main/kotlin/Double a Number Represented as a Linked List.kt
+++ kotlin/src/main/kotlin/Double a Number Represented as a Linked List.kt
@@ -1,5 +1,20 @@
 package o.mg
 
 fun doubleIt(head: ListNode?): ListNode? {
-    TODO()
+    val res = doubleItRemainder(head)
+    if (res > 0) {
+        val new = ListNode(res)
+        new.next = head
+        return new
+    }
+    return head
+}
+
+fun doubleItRemainder(node: ListNode?): Int {
+    if (node != null) {
+        val v = node.`val` * 2 + doubleItRemainder(node.next)
+        node.`val` = v % 10
+        return v/10
+    }
+    return 0
 }
