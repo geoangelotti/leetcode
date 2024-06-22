1ee694b4d2535b70ce6996c2fb7da8f11e71c5c0
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-22 18:13:09 +0200

    Merge Two Sorted Lists solution

--- kotlin/src/main/kotlin/Merge Two Sorted Lists.kt
+++ kotlin/src/main/kotlin/Merge Two Sorted Lists.kt
@@ -1,6 +1,15 @@
 package o.mg
 
 fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
-
-    TODO()
+    if (list1 == null || list2 == null) {
+        list1?.let { return list2 }
+        list2?.let { return list1 }
+        return null
+    }
+    if (list1.`val` > list2.`val`) {
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
+    }
+    list1.next = mergeTwoLists(list1.next, list2)
+    return list1
 }
