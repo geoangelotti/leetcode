7d515d6d85898a25e861922b904814ffe5d4a4c8
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-06-28 16:06:17 +0200

    Binary Search whitespace

--- kotlin/src/main/kotlin/Binary Search.kt
+++ kotlin/src/main/kotlin/Binary Search.kt
@@ -3,9 +3,9 @@ package o.mg
 fun search(nums: IntArray, target: Int): Int {
     var index = -1
     var start = 0
-    var end = nums.size -1
-    while(start <= end) {
-        val mid = (start+end)/2
+    var end = nums.size - 1
+    while (start <= end) {
+        val mid = (start + end) / 2
         if (nums[mid] == target) {
             index = mid
             break
@@ -13,7 +13,7 @@ fun search(nums: IntArray, target: Int): Int {
         if (nums[mid] < target) {
             start = mid + 1
         } else {
-            end = mid -1
+            end = mid - 1
         }
     }
     return index
