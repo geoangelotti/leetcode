f9d6dc0022df9e86150113f68a035221ad6ada8f
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-06-28 16:08:05 +0200

    Two Sum II - Input Array is Sorted whitespace

--- kotlin/src/test/kotlin/Two Sum II - Input Array Is Sorted Test.kt
+++ kotlin/src/test/kotlin/Two Sum II - Input Array Is Sorted Test.kt
@@ -4,20 +4,20 @@ import kotlin.test.assertEquals
 
 class `Two Sum II - Input Array Is Sorted Test` {
     @Test
-    fun case1(){
-        val result = twoSumII(intArrayOf(2,7,11,15), 9)
-        assertEquals(intArrayOf(1,2).toList(), result.toList())
+    fun case1() {
+        val result = twoSumII(intArrayOf(2, 7, 11, 15), 9)
+        assertEquals(intArrayOf(1, 2).toList(), result.toList())
     }
 
     @Test
-    fun case2(){
-        val result = twoSumII(intArrayOf(2,3,4), 6)
-        assertEquals(intArrayOf(1,3).toList(), result.toList())
+    fun case2() {
+        val result = twoSumII(intArrayOf(2, 3, 4), 6)
+        assertEquals(intArrayOf(1, 3).toList(), result.toList())
     }
 
     @Test
-    fun case3(){
-        val result = twoSumII(intArrayOf(-1,0), -1)
-        assertEquals(intArrayOf(1,2).toList(), result.toList())
+    fun case3() {
+        val result = twoSumII(intArrayOf(-1, 0), -1)
+        assertEquals(intArrayOf(1, 2).toList(), result.toList())
     }
 }
