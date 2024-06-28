c89471795b004ac11c37afe9efccd3a04158db62
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-06-28 16:41:36 +0200

    Find Center of Star Graph test

---
+++ kotlin/src/main/kotlin/Find Center of Star Graph.kt
@@ -0,0 +1,5 @@
+package o.mg
+
+fun findCenter(edges: Array<IntArray>): Int {
+    TODO()
+}
---
+++ kotlin/src/test/kotlin/Find Center of Star Graph Test.kt
@@ -0,0 +1,25 @@
+import o.mg.findCenter
+import java.util.stream.Stream
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.assertEquals
+
+class `Find Center of Star Graph Test` {
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    fun testMergeTwoLists(input: Array<IntArray>, expected: Int) {
+        val result = findCenter(input)
+        assertEquals(expected, result)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideTestCases(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(arrayOf(intArrayOf(1, 2), intArrayOf(2, 3), intArrayOf(4, 2)), 2),
+                Arguments.of(arrayOf(intArrayOf(1, 2), intArrayOf(5, 1), intArrayOf(1, 3), intArrayOf(1, 4)), 1),
+            )
+        }
+    }
+}
