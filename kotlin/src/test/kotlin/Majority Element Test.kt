0d5aba2f4c981211dc0eff3d5fa85e5a93ff98f4
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-06-28 16:08:46 +0200

    Majority Elements Test whitespace

--- kotlin/src/test/kotlin/Majority Element Test.kt
+++ kotlin/src/test/kotlin/Majority Element Test.kt
@@ -5,13 +5,13 @@ import kotlin.test.assertEquals
 class `Majority Element Test` {
     @Test
     fun case1() {
-        val result = majorityElement(intArrayOf(3,2,3))
+        val result = majorityElement(intArrayOf(3, 2, 3))
         assertEquals(3, result)
     }
 
     @Test
     fun case2() {
-        val result = majorityElement(intArrayOf(2,2,1,1,1,2,2))
+        val result = majorityElement(intArrayOf(2, 2, 1, 1, 1, 2, 2))
         assertEquals(2, result)
     }
 }
