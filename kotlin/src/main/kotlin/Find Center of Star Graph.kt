8982f4670d90749887ffa718ad305ede7d233b43
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-06-28 16:58:37 +0200

    Find Center of Star Graph solution
    serial traversal
    time O(nlogn), space O(nlogn)

--- kotlin/src/main/kotlin/Find Center of Star Graph.kt
+++ kotlin/src/main/kotlin/Find Center of Star Graph.kt
@@ -1,5 +1,11 @@
 package o.mg
 
 fun findCenter(edges: Array<IntArray>): Int {
-    TODO()
+    val nodes = mutableMapOf<Int, MutableSet<Int>>()
+    edges.forEach {
+        val (n1, n2) = it
+        nodes.getOrPut(n1) { mutableSetOf(n2) }.add(n2)
+        nodes.getOrPut(n2) { mutableSetOf(n1) }.add(n1)
+    }
+    return nodes.asSequence().sortedByDescending { it.value.size }.first().key
 }
