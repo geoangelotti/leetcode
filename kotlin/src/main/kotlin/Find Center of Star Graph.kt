01d578b94d4bb1686658c8b3b943d2aa173a1220
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-06-28 17:01:59 +0200

    Find Center of Star Graph solution
    greedy criterion the first 2 edges guarantee the existence of the central node
    time O(1), space O(1)

--- kotlin/src/main/kotlin/Find Center of Star Graph.kt
+++ kotlin/src/main/kotlin/Find Center of Star Graph.kt
@@ -1,11 +1,5 @@
 package o.mg
 
 fun findCenter(edges: Array<IntArray>): Int {
-    val nodes = mutableMapOf<Int, MutableSet<Int>>()
-    edges.forEach {
-        val (n1, n2) = it
-        nodes.getOrPut(n1) { mutableSetOf(n2) }.add(n2)
-        nodes.getOrPut(n2) { mutableSetOf(n1) }.add(n1)
-    }
-    return nodes.asSequence().sortedByDescending { it.value.size }.first().key
+    return if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) edges[0][0] else edges[0][1]
 }
