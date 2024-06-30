adb30057a2cd05c317389b048ae9846868461363
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-30 21:41:18 +0200

    First Missing Positive solution
    time O(n), space O(n)

--- kotlin/src/main/kotlin/First Missing Positive.kt
+++ kotlin/src/main/kotlin/First Missing Positive.kt
@@ -1,5 +1,10 @@
 package o.mg
 
 fun firstMissingPositive(nums: IntArray): Int {
-    TODO()
+    val set = nums.toSet()
+    var num = 1
+    while (num in set) {
+        num += 1
+    }
+    return num
 }
