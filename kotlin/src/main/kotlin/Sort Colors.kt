3218df269e3f8dadec78ac029c14cba7d661b0a6
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-30 18:25:42 +0200

    Sort Colors solution

--- kotlin/src/main/kotlin/Sort Colors.kt
+++ kotlin/src/main/kotlin/Sort Colors.kt
@@ -1,5 +1,19 @@
 package o.mg
 
 fun sortColors(nums: IntArray): Unit {
-    TODO()
+    val counter = IntArray(3) { 0 }
+    nums.forEach { counter[it] += 1 }
+    var rollingStart = 0
+    for (i in 0..<counter[0]) {
+        nums[i + rollingStart] = 0
+    }
+    rollingStart += counter[0]
+    for (i in 0..<counter[1]) {
+        nums[i + rollingStart] = 1
+    }
+    rollingStart += counter[1]
+    for (i in 0..<counter[2]) {
+        nums[i + rollingStart] = 2
+    }
+    rollingStart += counter[2]
 }
--- kotlin/src/test/kotlin/Sort Colors Test.kt
+++ kotlin/src/test/kotlin/Sort Colors Test.kt
@@ -1,10 +1,8 @@
-import o.mg.findCenter
 import o.mg.sortColors
 import java.util.stream.Stream
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
-import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class `Sort Colors Test` {
