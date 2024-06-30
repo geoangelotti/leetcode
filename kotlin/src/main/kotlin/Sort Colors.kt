4b29adf452a6337b17de9e2b92b767c75d887893
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-06-30 18:11:56 +0200

    Sort Colors test

---
+++ kotlin/src/main/kotlin/Sort Colors.kt
@@ -0,0 +1,5 @@
+package o.mg
+
+fun sortColors(nums: IntArray): Unit {
+    TODO()
+}
---
+++ kotlin/src/test/kotlin/Sort Colors Test.kt
@@ -0,0 +1,26 @@
+import o.mg.findCenter
+import o.mg.sortColors
+import java.util.stream.Stream
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.assertEquals
+
+class `Sort Colors Test` {
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    fun testSortColors(input: IntArray, expected: IntArray) {
+        sortColors(input)
+        assertEquals(expected, input)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideTestCases(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(intArrayOf(2, 0, 2, 1, 1, 0), intArrayOf(0, 0, 1, 1, 2, 2)),
+                Arguments.of(intArrayOf(2, 0, 1), intArrayOf(0, 1, 2)),
+            )
+        }
+    }
+}
