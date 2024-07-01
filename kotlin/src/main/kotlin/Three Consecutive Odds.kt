7479758108a83e79b08d3b54c4153c5f67910bae
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-07-01 11:03:22 +0200

    Three Consecutive Odds test

---
+++ kotlin/src/main/kotlin/Three Consecutive Odds.kt
@@ -0,0 +1,5 @@
+package o.mg
+
+fun threeConsecutiveOdds(arr: IntArray): Boolean {
+    TODO()
+}
---
+++ kotlin/src/test/kotlin/Three Consecutive Odds Test.kt
@@ -0,0 +1,26 @@
+import o.mg.threeConsecutiveOdds
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+import kotlin.test.assertEquals
+
+class `Three Consecutive Odds Test` {
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    fun testThreeConsecutiveOdds(input: IntArray, expected: Boolean) {
+        val result = threeConsecutiveOdds(input)
+        assertEquals(expected, result)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideTestCases(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(intArrayOf(2, 6, 4, 1), false),
+                Arguments.of(intArrayOf(1, 2, 34, 3, 4, 5, 7, 23, 12), true),
+                Arguments.of(intArrayOf(1, 1, 1), true),
+            )
+        }
+    }
+}
