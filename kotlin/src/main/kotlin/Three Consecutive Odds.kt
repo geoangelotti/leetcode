f10e83f4514fd309ad332f09e34582cf1b766142
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-07-01 11:07:50 +0200

    Three Consecutive Odds solution

--- kotlin/src/main/kotlin/Three Consecutive Odds.kt
+++ kotlin/src/main/kotlin/Three Consecutive Odds.kt
@@ -1,5 +1,9 @@
 package o.mg
 
 fun threeConsecutiveOdds(arr: IntArray): Boolean {
-    TODO()
+    for (i in 0..arr.size -3) {
+        if (arr[i] % 2 == 1 && arr[i+1] % 2 == 1 && arr[i+2] % 2 == 1)
+            return true
+    }
+    return false
 }
