286fe17172d9937f8640ae2af9464ff1063903e2
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-07-02 18:00:51 +0200

    Two Sum solution

--- scala/src/main/scala/TwoSum.scala
+++ scala/src/main/scala/TwoSum.scala
@@ -1,3 +1,9 @@
 def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-  throw NotImplementedError() 
+  def twoSum(index: Int, previous: Map[Int, Int]): Array[Int] = {
+    previous.get(target - nums(index)) match {
+      case Some(previousIndex) => Array(previousIndex, index)
+      case None => twoSum(index + 1, previous ++ Map(nums(index) -> index))
+    }
+  }
+  twoSum(0, Map.empty)
 }
