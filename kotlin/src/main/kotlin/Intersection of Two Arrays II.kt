e9d577462b997fcc6c473ebf00ee18074824151c
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-07-02 17:38:30 +0200

    Intersection of Two Arrays II solution

--- kotlin/src/main/kotlin/Intersection of Two Arrays II.kt
+++ kotlin/src/main/kotlin/Intersection of Two Arrays II.kt
@@ -1,5 +1,19 @@
 package o.mg
 
 fun intersectII(nums1: IntArray, nums2: IntArray): IntArray {
-    TODO()
+    val helper1 = IntArray(1001) { 0 }
+    val helper2 = IntArray(1001) { 0 }
+    nums1.forEach {
+        helper1[it]++
+    }
+    nums2.forEach {
+        helper2[it]++
+    }
+    val result = mutableListOf<Int>()
+    for (i in 0 until 1001) {
+        for (j in 0 until minOf(helper1[i], helper2[i])) {
+            result.add(i)
+        }
+    }
+    return result.toIntArray()
 }
