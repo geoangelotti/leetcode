fc948051addd911f51f3e86fd34877a206113987
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-07-05 19:05:59 +0200

    Find the Minimum and Maximum Number of Nodes Between Critical Points solution

--- kotlin/src/main/kotlin/Find the Minimum and Maximum Number of Nodes Between Critical Points.kt
+++ kotlin/src/main/kotlin/Find the Minimum and Maximum Number of Nodes Between Critical Points.kt
@@ -1,5 +1,28 @@
 package o.mg
 
 fun nodesBetweenCriticalPoints(head: ListNode?): IntArray {
-    TODO()
+    val criticalPoints = mutableListOf<Int>()
+    var idx = 1
+    var previous = head
+    var current = previous?.next
+    var next = current?.next
+    while (next != null) {
+        if ((current!!.`val` < previous!!.`val` && current.`val` < next.`val`)
+            || (current.`val` > previous.`val` && current.`val` > next.`val`)
+        ) {
+            criticalPoints.add(idx)
+        }
+        ++idx
+        previous = current
+        current = next
+        next = next.next
+    }
+    if (criticalPoints.size < 2) return intArrayOf(-1, -1)
+    val maxDistance = criticalPoints.last() - criticalPoints.first()
+    var minDistance = Int.MAX_VALUE
+    for (i in 1..criticalPoints.lastIndex) {
+        val distance = criticalPoints[i] - criticalPoints[i - 1]
+        minDistance = minOf(distance, minDistance)
+    }
+    return intArrayOf(minDistance, maxDistance)
 }
