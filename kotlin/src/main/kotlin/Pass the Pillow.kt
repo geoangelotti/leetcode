89753a84d9f0159d7c8ed2dd9c909a03a5340ef0
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-07-06 13:46:34 +0200

    Pass the Pillow solution

--- kotlin/src/main/kotlin/Pass the Pillow.kt
+++ kotlin/src/main/kotlin/Pass the Pillow.kt
@@ -1,5 +1,12 @@
 package o.mg
 
 fun passThePillow(n: Int, time: Int): Int {
-    TODO()
+    if (time < n) return time + 1
+    val allCountPasses = time % (n - 1)
+    val lastPass = time / (n - 1)
+    return when {
+        lastPass % 2 == 1 -> n - allCountPasses
+        else -> 1 + allCountPasses
+
+    }
 }
