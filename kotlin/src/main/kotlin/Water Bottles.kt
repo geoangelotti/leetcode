8dd38ed04f6ff562e43b3d68f00f6e2d386a50f3
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-07-07 22:51:25 +0200

    Water Bottles math solution

--- kotlin/src/main/kotlin/Water Bottles.kt
+++ kotlin/src/main/kotlin/Water Bottles.kt
@@ -1,11 +1,5 @@
 package o.mg
 
 fun numWaterBottles(numBottles: Int, numExchange: Int): Int {
-    var acc = numBottles
-    var empty = numBottles
-    while (empty >= numExchange) {
-        acc += empty / numExchange
-        empty = empty % numExchange + empty / numExchange
-    }
-    return acc
+    return numBottles + ((numBottles - 1) / (numExchange - 1))
 }
