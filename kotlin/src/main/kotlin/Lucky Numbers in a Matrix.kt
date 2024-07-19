120e9addf351fc6a70bd99df5359f92cf9f3a445
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-07-19 18:40:09 +0200

    whitespace

--- kotlin/src/main/kotlin/Lucky Numbers in a Matrix.kt
+++ kotlin/src/main/kotlin/Lucky Numbers in a Matrix.kt
@@ -1,27 +1,27 @@
 package o.mg
 
-fun luckyNumbers (matrix: Array<IntArray>): List<Int> {
-    val maxs = mutableMapOf<Pair<Int,Int>, Int>()
-    val mins = mutableMapOf<Pair<Int,Int>, Int>()
+fun luckyNumbers(matrix: Array<IntArray>): List<Int> {
+    val maxs = mutableMapOf<Pair<Int, Int>, Int>()
+    val mins = mutableMapOf<Pair<Int, Int>, Int>()
     val n = matrix.size
     val m = matrix[0].size
     for (row in 0..<n) {
-        var minPosition = Pair(0,row)
+        var minPosition = Pair(0, row)
         var min = matrix[row][0]
         for (col in 0..<m) {
             if (matrix[row][col] < min) {
-                minPosition = Pair(col,row)
+                minPosition = Pair(col, row)
                 min = matrix[row][col]
             }
         }
         mins[minPosition] = min
     }
     for (col in 0..<m) {
-        var maxPosition = Pair(col,0)
+        var maxPosition = Pair(col, 0)
         var max = matrix[0][col]
         for (row in 0..<n) {
             if (matrix[row][col] > max) {
-                maxPosition = Pair(col,row)
+                maxPosition = Pair(col, row)
                 max = matrix[row][col]
             }
         }
