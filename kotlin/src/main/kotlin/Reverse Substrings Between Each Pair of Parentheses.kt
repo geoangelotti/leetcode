64b2a07e0e89f46af41437346c5c3c06320c2e6c
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-07-20 20:39:08 +0200

    Reverse Substrings Between Each Pair of Parentheses test

---
+++ kotlin/src/main/kotlin/Reverse Substrings Between Each Pair of Parentheses.kt
@@ -0,0 +1,5 @@
+package o.mg
+
+fun reverseParentheses(s: String): String {
+    TODO()
+}
---
+++ kotlin/src/test/kotlin/Reverse Substrings Between Each Pair of Parentheses Test.kt
@@ -0,0 +1,26 @@
+import o.mg.reverseParentheses
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+import kotlin.test.assertEquals
+
+class `Reverse Substrings Between Each Pair of Parentheses Test` {
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    fun testReverseParentheses(s: String, expected: String) {
+        val result = reverseParentheses(s)
+        assertEquals(expected, result)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideTestCases(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("(abcd)", "dcba"),
+                Arguments.of("(u(love)i)", "iloveu"),
+                Arguments.of("(ed(et(oc))el)", "leetcode"),
+            )
+        }
+    }
+}
