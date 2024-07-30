6aa3c35a0162d73fc0ea794b12524fa894600d09
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-07-30 18:01:54 +0300

    Count Number of Teams solution

--- kotlin/src/main/kotlin/Count Number of Teams.kt
+++ kotlin/src/main/kotlin/Count Number of Teams.kt
@@ -1,5 +1,9 @@
 package o.mg
 
 fun numTeams(rating: IntArray): Int {
-    TODO()
+    return rating.foldIndexed(0) { i, t, x ->
+        val l = (0..<i).count { rating[it] < x }
+        val r = (i+1..rating.lastIndex).count { x < rating[it] }
+        t + l*r + (i-l)*(rating.lastIndex - r - i)
+    }
 }
