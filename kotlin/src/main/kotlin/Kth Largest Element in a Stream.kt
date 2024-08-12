3a3484c8a5e926550f9693cad000e0260e8a992e
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-08-12 14:35:47 +0300

    Kth Largest Element in a Stream solution

--- kotlin/src/main/kotlin/Kth Largest Element in a Stream.kt
+++ kotlin/src/main/kotlin/Kth Largest Element in a Stream.kt
@@ -1,7 +1,21 @@
 package o.mg
 
-class KthLargest(k: Int, nums: IntArray) {
+import java.util.PriorityQueue
+
+class KthLargest(private val k: Int, nums: IntArray) {
+    private val kSmallest = PriorityQueue<Int>()
+
+    init {
+        for (num in nums) {
+            add(num)
+        }
+    }
+
     fun add(`val`: Int): Int {
-        TODO()
+        kSmallest.offer(`val`)
+        if (kSmallest.size > k) {
+            kSmallest.poll()
+        }
+        return kSmallest.peek()
     }
 }
