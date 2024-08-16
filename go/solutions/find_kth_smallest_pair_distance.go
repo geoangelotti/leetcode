1a8c41bc94ad5b3374d13b6c8fdca889a5da9969
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-08-16 13:50:51 +0200

    Find K-th Smallest Pair Distance solution

--- go/solutions/find_kth_smallest_pair_distance.go
+++ go/solutions/find_kth_smallest_pair_distance.go
@@ -1,5 +1,28 @@
 package solutions
 
+import "sort"
+
 func SmallestDistancePair(nums []int, k int) int {
-	panic("Not implemented")
+	sort.Ints(nums)
+	low, high := 0, nums[len(nums)-1]-nums[0]
+	countPairs := func(nums []int, distance int) int {
+		count, left := 0, 0
+		for right := 1; right < len(nums); right++ {
+			for nums[right]-nums[left] > distance {
+				left++
+			}
+			count += right - left
+		}
+		return count
+	}
+	for low < high {
+		mid := low + (high-low)/2
+		if countPairs(nums, mid) < k {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+
+	return low
 }
