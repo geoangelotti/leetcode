1724ee4d781dce532bd06462529900ee82699a07
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-08-19 12:33:40 +0300

    Kth Distinct String in an Array whitespace

--- rust/src/kth_distinct_string_in_an_array.rs
+++ rust/src/kth_distinct_string_in_an_array.rs
@@ -25,8 +25,7 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case(vec![String::from("d"),
-    String::from("b"),String::from("c").to_string(),String::from("b"),String::from("c"),String::from("a")],2, String::from("a"))]
+    #[case(vec![String::from("d"), String::from("b"), String::from("c"), String::from("b"), String::from("c"), String::from("a")], 2, String::from("a"))]
     fn test_num_water_bottles(#[case] arr: Vec<String>, #[case] k: i32, #[case] expected: String) {
         let result = kth_distinct(arr, k);
         assert_eq!(result, expected);
