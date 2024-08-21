5bab83196fbe3d00725b88508051d12e6c3e37c3
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-08-21 09:24:35 +0200

    Maximum Distance in Arrays test

---
+++ go/solutions/maximum_distance_in_arrays.go
@@ -0,0 +1,5 @@
+package solutions
+
+func MaxDistance(arrays [][]int) int {
+	panic("Not implemented")
+}
---
+++ go/solutions/test/maximum_distance_in_arrays_test.go
@@ -0,0 +1,47 @@
+package solutions_test
+
+import (
+	"reflect"
+	"solutions"
+	"testing"
+)
+
+func TestMaxDistance(t *testing.T) {
+	type args struct {
+		arrays [][]int
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected int
+	}{
+		{
+			"case 1",
+			args{
+				[][]int{
+					{1, 2, 3},
+					{4, 5},
+					{1, 2, 3},
+				},
+			},
+			4,
+		},
+		{
+			"case 2",
+			args{
+				[][]int{
+					{1},
+					{1},
+				},
+			},
+			0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solutions.MaxDistance(tt.args.arrays); !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
+}
