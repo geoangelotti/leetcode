0fc8af650db056d8563a39825866ccd231a0cc09
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-08-25 16:17:05 +0200

    Binary Tree Postorder Traversal solution

--- kotlin/src/main/kotlin/Binary Tree Postorder Traversal.kt
+++ kotlin/src/main/kotlin/Binary Tree Postorder Traversal.kt
@@ -1,5 +1,12 @@
 package o.mg
 
 fun postorderTraversal(root: TreeNode?): List<Int> {
-   TODO()
+    if (root == null) {
+        return listOf()
+    }
+    val list1 = postorderTraversal(root.left).toMutableList()
+    val list2 = postorderTraversal(root.right)
+    list1.addAll(list2)
+    list1.add(root.`val`)
+    return list1
 }
