3d12014dd6bd0181814d7c0a9ad8d2d2006da6fb
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-08-25 16:15:22 +0200

    Binary Tree Postorder Traversal test

---
+++ kotlin/src/main/kotlin/Binary Tree Postorder Traversal.kt
@@ -0,0 +1,5 @@
+package o.mg
+
+fun postorderTraversal(root: TreeNode?): List<Int> {
+   TODO()
+}
---
+++ kotlin/src/test/kotlin/Binary Tree Postorder Traversal Test.kt
@@ -0,0 +1,36 @@
+import o.mg.TreeNode
+import o.mg.postorderTraversal
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class `Binary Tree Postorder Traversal Test` {
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    fun testPostorderTraversal(root: TreeNode?, expected: List<Int>) {
+        val result = postorderTraversal(root)
+        assertEquals(expected, result)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideTestCases(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(case1TreeNode(), listOf(3,2,1)),
+                Arguments.of(null, listOf<Int>()),
+                Arguments.of(TreeNode(1), listOf(1)),
+            )
+        }
+
+        private fun case1TreeNode(): TreeNode? {
+            val n2 = TreeNode(2)
+            n2.left = TreeNode(3)
+            val n1 = TreeNode(1)
+            n1.right = n2
+            println(n1)
+            return n1
+        }
+    }
+}
