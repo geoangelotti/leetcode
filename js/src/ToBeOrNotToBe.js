ed85af23ac001985143ccb7468b7d4ec0d04cd6e
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-08-26 18:33:13 +0200

    To Be Or Not To Be solution

--- js/src/ToBeOrNotToBe.js
+++ js/src/ToBeOrNotToBe.js
@@ -3,7 +3,20 @@
  * @return {Object}
  */
 var expect = function(val) {
-    
+    return {
+        toBe: (v) => {
+            if (v === val) {
+                return true;
+            }
+            throw new Error("Not Equal");
+        },
+        notToBe: (v) => {
+            if (v !== val) {
+                return true;
+            }
+            throw new Error("Equal");
+        },
+    };
 };
 
 module.exports = expect;
