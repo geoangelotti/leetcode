57db0236d8dff95d74136118d22ed060d7b4f693
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-08-26 09:13:45 +0200

    Fraction Addition and Subtraction solution

--- kotlin/src/main/kotlin/Fraction Addition and Subtraction.kt
+++ kotlin/src/main/kotlin/Fraction Addition and Subtraction.kt
@@ -1,5 +1,33 @@
 package o.mg
 
+import kotlin.math.abs
+
 fun fractionAddition(expression: String): String {
-    TODO()
+    var numerator = 0
+    var denominator = 1
+    var i = 0
+    while (i < expression.length) {
+        var sign = 1
+        if (expression[i] == '+' || expression[i] == '-') {
+            sign = if (expression[i] == '+') 1 else -1
+            i++
+        }
+        var numStart = i
+        while (expression[i] != '/') i++
+        val currentNumerator = sign * expression.substring(numStart, i).toInt()
+        i++
+        numStart = i
+        while (i < expression.length && expression[i].isDigit()) i++
+        val currentDenominator = expression.substring(numStart, i).toInt()
+        numerator = numerator * currentDenominator + currentNumerator * denominator
+        denominator *= currentDenominator
+        val gcd = gcd(abs(numerator), denominator)
+        numerator /= gcd
+        denominator /= gcd
+    }
+    return "$numerator/$denominator"
+}
+
+private fun gcd(a: Int, b: Int): Int {
+    return if (b == 0) a else gcd(b, a % b)
 }
