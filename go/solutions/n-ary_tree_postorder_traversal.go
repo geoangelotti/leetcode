723bb8c5caed86993bed775bcb0b6dcd98ef94df
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-08-27 16:49:13 +0200

    N-ary Tree Postorder Traversal solution

--- go/solutions/n-ary_tree_postorder_traversal.go
+++ go/solutions/n-ary_tree_postorder_traversal.go
@@ -1,5 +1,19 @@
 package solutions
 
 func Postorder(root *Node) []int {
-	return nil
+	post := []int{}
+	dfs(root, &post)
+	return post
+}
+
+func dfs(node *Node, post *[]int) {
+	if node == nil {
+		return
+	}
+	if len(node.Children) > 0 {
+		for _, c := range node.Children {
+			dfs(c, post)
+		}
+	}
+	*post = append(*post, node.Val)
 }
