1d8b29479c2d18288de1c404f09213a783f943e4
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-08-27 17:37:50 +0200

    N-ary Tree Preorder Traversal solution

--- go/solutions/n-ary_tree_preorder_traversal.go
+++ go/solutions/n-ary_tree_preorder_traversal.go
@@ -1,5 +1,14 @@
 package solutions
 
 func Preorder(root *Node) []int {
-	return nil
+	if root == nil {
+		return nil
+	}
+	var result []int
+	result = append(result, root.Val)
+	for _, child := range root.Children {
+		childRes := Preorder(child)
+		result = append(result, childRes...)
+	}
+	return result
 }
