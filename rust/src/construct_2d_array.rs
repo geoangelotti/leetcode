dd72451c87648d4ad5bfbc031abcc4e40c82e5f6
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-01 23:39:15 +0200

    Convert 1D Array Into 2D Array solution

--- rust/src/construct_2d_array.rs
+++ rust/src/construct_2d_array.rs
@@ -1,7 +1,16 @@
 #![allow(dead_code)]
 
 pub fn construct2_d_array(original: Vec<i32>, m: i32, n: i32) -> Vec<Vec<i32>> {
-    todo!()
+    if (m * n) as usize != original.len() {
+        return Vec::new();
+    }
+    let mut result = Vec::with_capacity(m as usize);
+    for i in 0..m {
+        let start = (i * n) as usize;
+        let end = start + n as usize;
+        result.push(original[start..end].to_vec());
+    }
+    result
 }
 
 #[cfg(test)]
