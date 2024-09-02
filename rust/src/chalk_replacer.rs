c3bb9e228ba818bb9b1b8acfb01f456ead1c9833
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-02 16:43:10 +0200

    Find the Student that Will Replace the Chalk even faster solution

--- rust/src/chalk_replacer.rs
+++ rust/src/chalk_replacer.rs
@@ -1,7 +1,10 @@
 #![allow(dead_code)]
 
 fn chalk_replacer(chalk: Vec<i32>, k: i32) -> i32 {
-    let chalk_needed: i64 = chalk.iter().map(|&x| x as i64).sum();
+    let chalk_needed: i64 = chalk
+        .iter()
+        .try_fold(0_i64, |acc, &c| acc.checked_add(c as _))
+        .unwrap();
     let mut remaining_chalk = (k as i64 % chalk_needed) as i32;
     for (i, &uses) in chalk.iter().enumerate() {
         if remaining_chalk < uses {
@@ -9,7 +12,6 @@ fn chalk_replacer(chalk: Vec<i32>, k: i32) -> i32 {
         }
         remaining_chalk -= uses;
     }
-
     0
 }
 
