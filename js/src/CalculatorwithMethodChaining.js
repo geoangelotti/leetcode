8af4b168acfd3b4e637691ab7f8b681a901cbd25
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-04 14:15:15 +0200

    Calculator with Method Chaining test

---
+++ js/src/CalculatorwithMethodChaining.js
@@ -0,0 +1,43 @@
+class Calculator {
+  /**
+   * @param {number} value
+   */
+  constructor(value) {}
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  add(value) {}
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  subtract(value) {}
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  multiply(value) {}
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  divide(value) {}
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  power(value) {}
+
+  /**
+   * @return {number}
+   */
+  getResult() {}
+}
+
+module.exports = Calculator;
