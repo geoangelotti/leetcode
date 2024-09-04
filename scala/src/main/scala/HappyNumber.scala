c0aedc30aa58b7678c73271846b275a246bae6f2
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-04 20:04:27 +0200

    Happy Number solution

--- scala/src/main/scala/HappyNumber.scala
+++ scala/src/main/scala/HappyNumber.scala
@@ -1,3 +1,9 @@
+import scala.annotation.tailrec
+
+@tailrec
 def isHappy(n: Int): Boolean = {
-  throw new Exception("Not implemented")
+  val h = n.toString.map(_.asDigit).map(a => a * a).sum
+  if (h == 1 || h == 7) true
+  else if (h <= 9) false
+  else isHappy(h)
 }
