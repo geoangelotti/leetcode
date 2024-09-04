bb0e8dac322f956af28838b6200394c4b31803cb
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-04 10:56:28 +0200

    Minimum Cost to Convert String I test

---
+++ go/solutions/minimum_cost_to_convert_string_I.go
@@ -0,0 +1,5 @@
+package solutions
+
+func MinimumCost(source, target string, original, changed []byte, cost []int) (ans int64) {
+	panic("Not implemented")
+}
---
+++ go/solutions/test/minimum_cost_to_convert_string_I_test.go
@@ -0,0 +1,63 @@
+package solutions_test
+
+import (
+	"reflect"
+	"solutions"
+	"testing"
+)
+
+func TestMinimumCost(t *testing.T) {
+	type args struct {
+		source   string
+		target   string
+		original []byte
+		changed  []byte
+		cost     []int
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected int
+	}{
+		{
+			"case1",
+			args{
+				"abcd",
+				"acbe",
+				[]byte{'a', 'b', 'c', 'c', 'e', 'd'},
+				[]byte{'b', 'c', 'b', 'e', 'b', 'e'},
+				[]int{2, 5, 5, 1, 2, 20},
+			},
+			28,
+		},
+		{
+			"case1",
+			args{
+				"aaaa",
+				"bbbb",
+				[]byte{'a', 'c'},
+				[]byte{'c', 'b'},
+				[]int{1, 2},
+			},
+			12,
+		},
+		{
+			"case1",
+			args{
+				"abcd",
+				"abce",
+				[]byte{'a'},
+				[]byte{'e'},
+				[]int{1000},
+			},
+			-1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solutions.MinimumCost(tt.args.source, tt.args.target, tt.args.original, tt.args.changed, tt.args.cost); !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("MinimumCost(%v, %v, %v, %v, %v) = %v, want %v", tt.args.source, tt.args.target, tt.args.original, tt.args.changed, tt.args.cost, got, tt.expected)
+			}
+		})
+	}
+}
