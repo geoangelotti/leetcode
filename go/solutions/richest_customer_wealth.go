f8903477562f5d62a633bb61761e087e40361a03
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-05 13:21:44 +0200

    Richest Customer Wealth solution

--- go/solutions/richest_customer_wealth.go
+++ go/solutions/richest_customer_wealth.go
@@ -1,5 +1,15 @@
 package solutions
 
 func MaximumWealth(accounts [][]int) int {
-	panic("Not impemented")
+	var max int = 0
+	for _, customer := range accounts {
+		var acc = 0
+		for _, account := range customer {
+			acc += account
+		}
+		if acc > max {
+			max = acc
+		}
+	}
+	return max
 }
