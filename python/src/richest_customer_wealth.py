34ebd6f8d1436d8d16229ed49b94387e5d49f90b
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-05 13:48:57 +0200

    Richest Customer Wealth solution

--- python/src/richest_customer_wealth.py
+++ python/src/richest_customer_wealth.py
@@ -1,5 +1,5 @@
 from typing import List
 
 
-def maximumWealth(self, accounts: List[List[int]]) -> int:
-    pass
+def maximumWealth(accounts: List[List[int]]) -> int:
+    return max(sum(row) for row in accounts)
