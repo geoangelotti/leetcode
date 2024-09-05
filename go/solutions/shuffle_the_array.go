89e432029e4e65b8441adc61b10e1b4ec8e01a65
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-05 11:29:50 +0200

    Shuffle the Array solution

--- go/solutions/shuffle_the_array.go
+++ go/solutions/shuffle_the_array.go
@@ -1,5 +1,19 @@
 package solutions
 
 func Shuffle(nums []int, n int) []int {
-	panic("Not implemented")
+	n1 := []int{}
+	n2 := []int{}
+	for i, v := range nums {
+		if i < n {
+			n1 = append(n1, v)
+		} else {
+			n2 = append(n2, v)
+		}
+	}
+	res := []int{}
+	for i := 0; i < n; i++ {
+		res = append(res, n1[i])
+		res = append(res, n2[i])
+	}
+	return res
 }
