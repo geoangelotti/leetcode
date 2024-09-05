6437d8954d2ca7fc85a8f190c6254510fff317f7
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-05 23:48:48 +0200

    Calculator with Method Chaining test

--- js/test/CalculatorwithMethodChaining.test.js
+++ js/test/CalculatorwithMethodChaining.test.js
@@ -3,13 +3,13 @@ const assert = require("node:assert/strict");
 const Calculator = require("../src/CalculatorwithMethodChaining");
 
 test("Calculator with Method Chaining", async (t) => {
-  await t.test("", () => {
+  await t.test("add", () => {
     assert.strictEqual(new Calculator(10).add(5).subtract(7).getResult(), 8);
   });
-  await t.test("", () => {
+  await t.test("power", () => {
     assert.strictEqual(new Calculator(2).multiply(5).power(2).getResult(), 100);
   });
-  await t.test("", () => {
+  await t.test("division error", () => {
     assert.throws(
       () => {
         new Calculator(20).divide(0);
