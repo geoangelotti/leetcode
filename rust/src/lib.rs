2d3631ef840ffde2ac41cc1f38aa56be405f488b
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-05 13:29:32 +0200

    Water Bottles rename

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,7 +3,7 @@ mod construct_2d_array;
 mod count_integers_with_even_digit_sum;
 mod invert_binary_tree;
 mod kth_distinct_string_in_an_array;
-mod num_water_bottles;
 mod sum_of_digits_of_string_after_convert;
 mod tree_node;
 mod ugly_number_2;
+mod water_bottles;
