8cf55bfb591f80a59735d2de70fea0c178b779d2
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-05 12:24:55 +0200

    Find the City With the Smallest Number of Neighbors at a Threshold Distance solution

--- go/solutions/find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance.go
+++ go/solutions/find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance.go
@@ -1,5 +1,40 @@
 package solutions
 
 func FindTheCity(n int, edges [][]int, distanceThreshold int) int {
-	panic("Not implemented")
+	dist := make([][]int, n)
+	for i := range dist {
+		dist[i] = make([]int, n)
+		for j := range dist[i] {
+			dist[i][j] = 10001
+		}
+		dist[i][i] = 0
+	}
+	for _, edge := range edges {
+		dist[edge[0]][edge[1]] = edge[2]
+		dist[edge[1]][edge[0]] = edge[2]
+	}
+	for k := 0; k < n; k++ {
+		for i := 0; i < n; i++ {
+			for j := 0; j < n; j++ {
+				if dist[i][k]+dist[k][j] < dist[i][j] {
+					dist[i][j] = dist[i][k] + dist[k][j]
+				}
+			}
+		}
+	}
+	minReachableCities := n
+	result := -1
+	for i := 0; i < n; i++ {
+		reachableCities := 0
+		for j := 0; j < n; j++ {
+			if dist[i][j] <= distanceThreshold {
+				reachableCities++
+			}
+		}
+		if reachableCities <= minReachableCities {
+			minReachableCities = reachableCities
+			result = i
+		}
+	}
+	return result
 }
