936299fb484f92642bc819b7fba13ceeb1bbe40b
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-06 17:33:56 +0200

    Richest Customer Wealth solution

--- cpp/src/richest_customer_wealth.cpp
+++ cpp/src/richest_customer_wealth.cpp
@@ -1,6 +1,14 @@
 #include <vector>
+#include <numeric>
 
 int maximumWealth(std::vector<std::vector<int>> &accounts)
 {
-	return 0;
+	return std::transform_reduce(
+		accounts.cbegin(),
+		accounts.cend(),
+		0,
+		[](auto a, auto b)
+		{ return std::max(a, b); },
+		[](auto const &row)
+		{ return std::reduce(row.cbegin(), row.cend()); });
 }
