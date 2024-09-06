231a5e3cb9f183bb3520f1009f2b6dda7b2f5b89
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-06 16:03:43 +0200

    Happy Number solution
    2 pointers approach

--- kotlin/src/main/kotlin/Happy Number.kt
+++ kotlin/src/main/kotlin/Happy Number.kt
@@ -1,33 +1,17 @@
 package o.mg
 
-private fun digits(n: Int): List<Int> {
-    val array = mutableListOf<Int>()
-    var n = n
-    var r = n
-    while (n >= 1) {
-        r = n % 10
-        n /= 10
-        array.add(r)
-    }
-    if (array.isEmpty()) {
-        array.add(r)
-    }
-    return array.asReversed()
-}
-
-private fun isHappyMore(n: Int, s: MutableSet<Int>): Boolean {
-    if (s.contains(n)) {
-        return false
-    }
-    s.add(n)
-    val digits = digits(n).sumOf { it * it }
-    if (digits == 1) {
-        return true
-    }
-    return isHappyMore(digits, s)
+private fun digitsSquared(n: Int): Int {
+   return n.toString().toCharArray().sumOf { it.digitToInt() * it.digitToInt() }
 }
 
 fun isHappy(n: Int): Boolean {
-    return isHappyMore(n, mutableSetOf())
+    if (n == 1) return true
+    var slow = digitsSquared(n)
+    var fast = digitsSquared(digitsSquared(n))
+    while (slow != fast) {
+        slow = digitsSquared(slow)
+        fast = digitsSquared(digitsSquared(fast))
+    }
+    return slow == 1
 }
 
