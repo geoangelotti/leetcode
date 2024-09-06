234a911d35ec36cdb560da0169f1d5aa3a7adfa3
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-06 15:18:26 +0200

    Determine if String Halves Are Alike solution

--- kotlin/src/main/kotlin/Determine if String Halves Are Alike.kt
+++ kotlin/src/main/kotlin/Determine if String Halves Are Alike.kt
@@ -1,6 +1,7 @@
 package o.mg
 
 fun halvesAreAlike(s: String): Boolean {
+    val vowels = setOf('a', 'e', 'i', 'o', 'u')
     val s1 = s.substring(0, s.length / 2)
     val s2 = s.substring(s.length / 2)
     return s1.count { vowels.contains(it.lowercaseChar()) } == s2.count { vowels.contains(it.lowercaseChar()) }
