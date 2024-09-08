c2304b887279afdb3671642ff70433bf4f4ac43e
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-08 21:19:45 +0200

    Most Profit Assigning Work solution

--- kotlin/src/main/kotlin/Most Profit Assigning Work.kt
+++ kotlin/src/main/kotlin/Most Profit Assigning Work.kt
@@ -1,5 +1,14 @@
 package o.mg
 
+import kotlin.math.max
+
 fun maxProfitAssignment(difficulty: IntArray, profit: IntArray, worker: IntArray): Int {
-   TODO()
+   val indices = profit.indices.sortedBy { difficulty[it] }
+   var maxProfit = 0
+   var i = 0
+   return worker.sorted().sumOf { d ->
+      while (i < indices.size && difficulty[indices[i]] <= d)
+         maxProfit = max(maxProfit, profit[indices[i++]])
+      maxProfit
+   }
 }
