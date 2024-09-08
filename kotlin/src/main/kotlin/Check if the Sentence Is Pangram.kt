39c8b96b155f8c7e07d6b68e850b0b435368ba78
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-08 20:41:29 +0200

    Check if the Sentence Is Pangram solution

--- kotlin/src/main/kotlin/Check if the Sentence Is Pangram.kt
+++ kotlin/src/main/kotlin/Check if the Sentence Is Pangram.kt
@@ -1,5 +1,14 @@
 package o.mg
 
 fun checkIfPangram(sentence: String): Boolean {
-    TODO()
+    val map = IntArray(26) { 0 }
+    sentence.forEach {
+        map[it - 'a'] += 1
+    }
+    map.forEach {
+        if (it == 0) {
+            return false
+        }
+    }
+    return true
 }
