b3dc1008df19852edee68590c150762bf859d475
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-08 21:26:39 +0200

    Maximum Score From Removing Substrings solution

--- kotlin/src/main/kotlin/Maximum Score From Removing Substrings.kt
+++ kotlin/src/main/kotlin/Maximum Score From Removing Substrings.kt
@@ -1,5 +1,29 @@
 package o.mg
 
+import java.util.*
+
 fun maximumGain(s: String, x: Int, y: Int): Int {
-    TODO()
+    val stack1 = Stack<Char>()
+    val stack2 = Stack<Char>()
+    val first = if (x>y) 'a' else 'b'
+    val second = if (x>y) 'b' else 'a'
+    var result = 0
+    s.forEach { char ->
+        if (stack1.isNotEmpty() && stack1.peek() == first && char == second) {
+            stack1.pop()
+            result += maxOf(x,y)
+        } else {
+            stack1.push(char)
+        }
+    }
+    while(stack1.isNotEmpty()) {
+        val char = stack1.pop()
+        if (stack2.isNotEmpty() && stack2.peek() == first && char == second) {
+            stack2.pop()
+            result += minOf(x, y)
+        } else {
+            stack2.push(char)
+        }
+    }
+    return result
 }
