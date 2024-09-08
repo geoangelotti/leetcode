c6a4b9bcb89e4a9a1a483a3a55e40869a484c192
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-08 16:12:31 +0200

    Invert Binary Tree solution

--- c/src/invert_binary_tree.c
+++ c/src/invert_binary_tree.c
@@ -3,5 +3,15 @@
 
 struct TreeNode *invertTree(struct TreeNode *root)
 {
-	return NULL;
+	if (root == NULL)
+	{
+		return NULL;
+	}
+	struct TreeNode *left = root->left;
+	struct TreeNode *right = root->right;
+	root->left = right;
+	root->right = left;
+	invertTree(root->left);
+	invertTree(root->right);
+	return root;
 }
