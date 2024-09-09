6269c599a46f10adacd04930abfaa82bf38a309c
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-09 20:43:00 +0200

    Number of Senior Citizens test typo

--- python/tests/test_number_of_senior_citizens.py
+++ python/tests/test_number_of_senior_citizens.py
@@ -7,5 +7,5 @@ from src.number_of_senior_citizens import countSeniors
     (["7868190130M7522", "5303914400F9211", "9273338290F4010"], 2),
     (["1313579440F2036", "2921522980M5644"], 0)
 ])
-def testCountSenios(input: List[str], expected: int):
+def testCountSeniors(input: List[str], expected: int):
     assert countSeniors(input) == expected
