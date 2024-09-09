9bdb74aac9007a17c2bae20e796326cb72c4d202
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-09 11:46:41 +0200

    Number of Good Leaf Nodes Pairs solution

--- kotlin/src/main/kotlin/Number of Good Leaf Nodes Pairs.kt
+++ kotlin/src/main/kotlin/Number of Good Leaf Nodes Pairs.kt
@@ -1,5 +1,45 @@
 package o.mg
 
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
+
 fun countPairs(root: TreeNode, distance: Int): Int {
-    TODO()
+    val leaves = HashSet<TreeNode>()
+    val parents = HashMap<TreeNode, TreeNode>()
+    val queue = LinkedList<TreeNode>()
+    queue.add(root)
+    while (queue.isNotEmpty()) {
+        val node = queue.pop()
+        if (node.left == null && node.right == null) {
+            leaves.add(node)
+        }
+        node.left?.let {
+            queue.add(it)
+            parents[it] = node
+        }
+        node.right?.let {
+            queue.add(it)
+            parents[it] = node
+        }
+    }
+    var pairsCount = 0
+    for (leaf in leaves) {
+        val queue = LinkedList<Pair<TreeNode?, TreeNode>>()
+        queue.add(null to leaf)
+        var stepsCount = 0
+        while (queue.isNotEmpty() && stepsCount <= distance) {
+            repeat(queue.size) {
+                val (from, node) = queue.pop()
+                if (node != leaf && leaves.contains(node)) {
+                    pairsCount++
+                }
+                parents[node]?.takeIf { it != from }?.let { queue.add(node to it) }
+                node.left?.takeIf { it != from }?.let { queue.add(node to it) }
+                node.right?.takeIf { it != from }?.let { queue.add(node to it) }
+            }
+            stepsCount++
+        }
+    }
+    return pairsCount / 2
 }
