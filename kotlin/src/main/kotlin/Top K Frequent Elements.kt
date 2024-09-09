593c6bc316c5b183d3a483c02f0726c6d681bc72
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-09 20:06:12 +0200

    Top K Frequent Elements solution

--- kotlin/src/main/kotlin/Top K Frequent Elements.kt
+++ kotlin/src/main/kotlin/Top K Frequent Elements.kt
@@ -1,5 +1,5 @@
 package o.mg
 
-fun topKFrequent(nums: IntArray, k: Int): IntArray {
-    TODO()
-}
+fun topKFrequent(nums: IntArray, k: Int): IntArray =
+    nums.asSequence().groupingBy { it }.eachCount().asSequence().sortedByDescending { it.value }.map { it.key }.take(k)
+        .toList().toIntArray()
