9d02e1a97fc6dbb264c058bfa4cb19de951b7250
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-10 18:22:02 +0200

    Split Linked List in Parts fix test

--- go/solutions/test/split_linked_list_in_parts_test.go
+++ go/solutions/test/split_linked_list_in_parts_test.go
@@ -34,7 +34,7 @@ func TestSplitListToParts(t *testing.T) {
 			"case2",
 			args{
 				solutions.NewListNode([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}),
-				10,
+				3,
 			},
 			[]*solutions.ListNode{
 				solutions.NewListNode([]int{1, 2, 3, 4}),
