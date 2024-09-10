bc90dd734a18f2e2779fc5b49d3b833c33b797c2
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-10 17:32:09 +0200

    Insert Greatest Common Divisors in Linked List solution

--- go/solutions/insert_greatest_common_divisors_in_linked_list.go
+++ go/solutions/insert_greatest_common_divisors_in_linked_list.go
@@ -1,5 +1,26 @@
 package solutions
 
 func InsertGreatestCommonDivisors(head *ListNode) *ListNode {
-	panic("Not implemented")
+	if head == nil {
+		return nil
+	}
+	current := head
+	for current.Next != nil {
+		next := current.Next
+		gcd := ListNode{Val: greatestCommonDivisors(current.Val, next.Val), Next: next}
+		current.Next = &gcd
+		println(&current)
+		current = next
+	}
+	return head
+}
+
+func greatestCommonDivisors(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	temp := a
+	a = b
+	b = temp % a
+	return greatestCommonDivisors(a, b)
 }
