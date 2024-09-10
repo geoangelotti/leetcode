e21fd253f2b13bb5d1f74b4e032d243c13e3d1ed
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-10 15:20:57 +0200

    Majority Element II solution

--- python/src/majority_element_ii.py
+++ python/src/majority_element_ii.py
@@ -1,5 +1,6 @@
 from typing import List
+from itertools import groupby
 
 
 def majorityElement(nums: List[int]) -> List[int]:
-    pass
+    return [k for k, v in {k: sum(1 for _ in g) for k, g in groupby(sorted(nums))}.items() if v > len(nums)/3]
