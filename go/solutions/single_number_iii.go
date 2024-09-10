08973cbd55cb46ac089f3c5b33a7d27bcb346682
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-10 10:43:26 +0200

    Single Number III solution

--- go/solutions/single_number_iii.go
+++ go/solutions/single_number_iii.go
@@ -1,5 +1,17 @@
 package solutions
 
 func SingleNumberIII(nums []int) []int {
-	panic("Not implemented")
+	res := []int{}
+	m := make(map[int]int)
+	for _, v := range nums {
+		s := m[v]
+		s++
+		m[v] = s
+	}
+	for k, v := range m {
+		if v == 1 {
+			res = append(res, k)
+		}
+	}
+	return res
 }
