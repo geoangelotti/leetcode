81934fb1a894ba159b574e88a5555fe5ffc3c248
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-10 15:04:22 +0200

    Climbing Stairs solution

--- go/solutions/climbing_stairs.go
+++ go/solutions/climbing_stairs.go
@@ -1,5 +1,13 @@
 package solutions
 
 func ClimbStairs(n int) int {
-	panic("Not implemented")
+	var a, b int
+	a = 1
+	b = 1
+	for i := 0; i < n-1; i++ {
+		temp := a
+		a = a + b
+		b = temp
+	}
+	return a
 }
