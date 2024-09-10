d0e9b869db531a728d9436ce067fcb4f604c6646
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-10 11:21:46 +0200

    Single Number II solution

--- go/solutions/single_number_ii.go
+++ go/solutions/single_number_ii.go
@@ -1,5 +1,16 @@
 package solutions
 
 func SingleNumberII(nums []int) int {
-	panic("Not implemented")
+	m := make(map[int]int)
+	for _, v := range nums {
+		s := m[v]
+		s++
+		m[v] = s
+	}
+	for k, v := range m {
+		if v == 1 {
+			return k
+		}
+	}
+	return 0
 }
