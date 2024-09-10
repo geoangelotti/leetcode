88295311d4938c2a2e0406d7bddaf265e681d42a
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-10 18:22:16 +0200

    Split Linked List in Parts solution

--- go/solutions/split_linked_list_in_parts.go
+++ go/solutions/split_linked_list_in_parts.go
@@ -1,5 +1,31 @@
 package solutions
 
 func SplitListToParts(head *ListNode, k int) []*ListNode {
-	panic("Not implemented")
+	current := head
+	var length int
+	for current != nil {
+		length += 1
+		current = current.Next
+	}
+	result := make([]*ListNode, k)
+	quotient := length / k
+	remainder := length % k
+	var i int
+	current = head
+	for current != nil {
+		result[i] = current
+		currentLength := quotient
+		if remainder > 0 {
+			currentLength += 1
+			remainder -= 1
+		}
+		for i := 0; i < currentLength-1; i++ {
+			current = current.Next
+		}
+		temp := current.Next
+		current.Next = nil
+		current = temp
+		i++
+	}
+	return result
 }
