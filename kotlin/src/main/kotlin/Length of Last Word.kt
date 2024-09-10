2c73ac344f7eaa5ab9ff77bbe084643931afe21c
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-10 19:51:16 +0200

    Length of Last Word solution
    remove method chaining

--- kotlin/src/main/kotlin/Length of Last Word.kt
+++ kotlin/src/main/kotlin/Length of Last Word.kt
@@ -1,5 +1,5 @@
 package o.mg
 
 fun lengthOfLastWord(s: String): Int {
-    return s.split(" ").filter{ !it.isEmpty() && !it.isBlank() }.last().length
+    return s.split(" ").last { it.isNotEmpty() && it.isNotBlank() }.length
 }
