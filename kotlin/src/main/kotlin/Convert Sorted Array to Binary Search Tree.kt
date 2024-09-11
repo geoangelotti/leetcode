4a5cb28df13b4afa8d38f131aeca4e01c3f62140
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-11 16:09:10 +0200

    Convert Sorted Array to Binary Search Tree solution

--- kotlin/src/main/kotlin/Convert Sorted Array to Binary Search Tree.kt
+++ kotlin/src/main/kotlin/Convert Sorted Array to Binary Search Tree.kt
@@ -1,5 +1,16 @@
 package o.mg
 
 fun sortedArrayToBST(nums: IntArray): TreeNode? {
-    TODO()
+    return sortedArrayToBSTIndex(nums, 0, nums.size - 1)
+}
+
+fun sortedArrayToBSTIndex(nums: IntArray, left: Int, right: Int): TreeNode? {
+    if (left > right) {
+        return null
+    }
+    val half = (left + right) / 2
+    val node = TreeNode(nums[half])
+    node.left = sortedArrayToBSTIndex(nums, left, half - 1)
+    node.right = sortedArrayToBSTIndex(nums, half + 1, right)
+    return node
 }
