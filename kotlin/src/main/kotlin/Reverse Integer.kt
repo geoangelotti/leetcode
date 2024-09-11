61ed40c1e4b32f1a12d0545b9dfc2450a3ee867b
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-11 11:20:09 +0200

    Reverse Integer solution

--- kotlin/src/main/kotlin/Reverse Integer.kt
+++ kotlin/src/main/kotlin/Reverse Integer.kt
@@ -1,5 +1,12 @@
 package o.mg
 
+import kotlin.math.abs
+
 fun reverse(x: Int): Int {
-    TODO()
+    val s = x.toString().filter { it.isDigit() }
+    val reversed = s.reversed().toLong()
+    if (abs(reversed) > Int.MAX_VALUE) {
+        return 0
+    }
+    return ((if (x > 0) 1 else -1) * s.reversed().toLong()).toInt()
 }
