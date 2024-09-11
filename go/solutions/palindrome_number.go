7e24b9c314784a227a16581c1a6297b837b243d9
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-11 11:45:49 +0200

    Palindrome Number solution

--- go/solutions/palindrome_number.go
+++ go/solutions/palindrome_number.go
@@ -1,5 +1,16 @@
 package solutions
 
+import "strconv"
+
 func IsPalindrome(x int) bool {
-	panic("Not implemented")
+	if x < 0 {
+		return false
+	}
+	str := strconv.Itoa(x)
+	for i := 0; i < len(str)/2; i++ {
+		if str[i] != str[len(str)-1-i] {
+			return false
+		}
+	}
+	return true
 }
