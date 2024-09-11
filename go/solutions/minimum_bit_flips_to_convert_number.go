9e061c4e1254e705bde38b2dad6c41cd7ba0ce70
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-11 20:28:58 +0200

    Minimum Bit Flips to Convert Number solution

--- go/solutions/minimum_bit_flips_to_convert_number.go
+++ go/solutions/minimum_bit_flips_to_convert_number.go
@@ -1,5 +1,11 @@
 package solutions
 
 func MinBitFlips(start int, goal int) int {
-	panic("Not implemented")
+	n := start ^ goal
+	var count int
+	for n > 0 {
+		count += (n & 1)
+		n >>= 1
+	}
+	return count
 }
