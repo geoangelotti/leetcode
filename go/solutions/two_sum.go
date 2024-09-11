7b4a094cbf54a285b6528d91e7670a0e9593f900
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-11 11:31:44 +0200

    Two Sum solution

--- go/solutions/two_sum.go
+++ go/solutions/two_sum.go
@@ -1,5 +1,17 @@
 package solutions
 
 func TwoSum(nums []int, target int) []int {
-	panic("Not implemented")
+	var indexes = make(map[int]int)
+	for i, value := range nums {
+		diff := target - value
+		v, exists := indexes[diff]
+		if exists {
+			ret := [2]int{0, 0}
+			ret[0] = v
+			ret[1] = i
+			return ret[:]
+		}
+		indexes[value] = i
+	}
+	return []int{}
 }
