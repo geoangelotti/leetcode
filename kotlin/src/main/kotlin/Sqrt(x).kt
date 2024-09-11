556a27c7fb9fc3165c2484f256f1b4c408c1f625
Georgios Angelopoulos <g.angelopoulos@rewe-group.at>
2024-09-11 09:48:57 +0200

    Sqrt(x) solution

--- kotlin/src/main/kotlin/Sqrt(x).kt
+++ kotlin/src/main/kotlin/Sqrt(x).kt
@@ -1,5 +1,16 @@
 package o.mg
 
 fun mySqrt(x: Int): Int {
-    TODO()
+    if (x == 0 || x == 1) return x
+    var l = 1
+    var r = x
+    while (l <= r) {
+        val mid = l + (r - l) / 2
+        if (mid.toLong() * mid > x.toLong()) r = mid - 1
+        else if (mid * mid == x)
+            return mid
+        else
+            l = mid + 1
+    }
+    return r
 }
