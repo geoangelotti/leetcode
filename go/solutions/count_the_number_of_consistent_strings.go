eb7de9eec13dfb3290127e9a8d31672302df7cc0
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-12 17:30:38 +0200

    Count the Number of Consistent Strings solution

--- go/solutions/count_the_number_of_consistent_strings.go
+++ go/solutions/count_the_number_of_consistent_strings.go
@@ -1,5 +1,21 @@
 package solutions
 
 func CountConsistentStrings(allowed string, words []string) int {
-	panic("Not implemented")
+	var c int
+	allows := make([]rune, 26)
+	for _, c := range allowed {
+		allows[c-'a'] = 1
+	}
+	for _, word := range words {
+		consistent := true
+		for _, c := range word {
+			if allows[c-'a'] == 0 {
+				consistent = false
+			}
+		}
+		if consistent {
+			c += 1
+		}
+	}
+	return c
 }
