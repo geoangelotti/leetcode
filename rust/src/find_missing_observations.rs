1aeebea2b355ed2600383e5d6f37356189e9e842
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-12 15:01:15 +0200

    Find Missing Observations solution

--- rust/src/find_missing_observations.rs
+++ rust/src/find_missing_observations.rs
@@ -1,5 +1,13 @@
+#![allow(dead_code)]
+
 fn missing_rolls(rolls: Vec<i32>, mean: i32, n: i32) -> Vec<i32> {
-    todo!()
+    let x = mean * (n + rolls.len() as i32) - rolls.iter().sum::<i32>();
+    if x < n || x > n * 6 {
+        return vec![];
+    }
+    (0..n as usize)
+        .map(|i| x / n + (x % n > i as i32) as i32)
+        .collect()
 }
 
 #[cfg(test)]
@@ -9,7 +17,7 @@ mod tests {
 
     #[rstest]
     #[case(vec![3, 2, 4, 3], 4, 2, vec![6, 6])]
-    #[case(vec![1, 5, 6], 3, 4, vec![2, 3, 2, 2])]
+    #[case(vec![1, 5, 6], 3, 4, vec![3, 2, 2, 2])]
     #[case(vec![1, 2, 3, 4], 6, 4, vec![])]
     fn test_missing_rolls(
         #[case] rolls: Vec<i32>,
