27aa9a8fda4b70696ffd7c4ea7aed409f1f2b498
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-12 11:01:14 +0200

    First Missing Positive solution

--- python/src/first_missing_positive.py
+++ python/src/first_missing_positive.py
@@ -2,4 +2,8 @@ from typing import List
 
 
 def firstMissingPositive(nums: List[int]) -> int:
-    pass
+    s = {x for x in nums}
+    small = 1
+    while small in s:
+        small += 1
+    return small
