72ea20b9e43bb996a759f7d2a55c32ce1ee9b173
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-14 17:30:04 +0200

    XOR Queries of a Subarray solution

--- go/solutions/xor_queries_of_a_subarray.go
+++ go/solutions/xor_queries_of_a_subarray.go
@@ -1,5 +1,24 @@
 package solutions
 
 func XorQueries(arr []int, queries [][]int) []int {
-	panic("Not implemented")
+	if arr == nil {
+		return nil
+	}
+	prefix := make([]int, 0)
+	prefix = append(prefix, arr[0])
+
+	for i := 1; i < len(arr); i++ {
+		prefix = append(prefix, arr[i]^prefix[i-1])
+	}
+	res := make([]int, 0)
+	for _, query := range queries {
+		start := query[0]
+		end := query[1]
+		if start == 0 {
+			res = append(res, prefix[end])
+		} else {
+			res = append(res, prefix[end]^prefix[start-1])
+		}
+	}
+	return res
 }
