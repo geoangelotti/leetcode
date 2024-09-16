ff5e05d2c75dbaa3e59c78454d3e4fa3bb7ba449
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-16 23:12:37 +0200

    Minimum Time Difference solution

--- rust/src/minimum_time_difference.rs
+++ rust/src/minimum_time_difference.rs
@@ -1,7 +1,13 @@
 #![allow(dead_code)]
 
 fn find_min_difference(time_points: Vec<String>) -> i32 {
-    todo!()
+    let mut v = Vec::with_capacity(time_points.len() + 1);
+    for s in time_points.iter() {
+        v.push(s[..2].parse::<i32>().unwrap() * 60 + s[3..].parse::<i32>().unwrap());
+    }
+    v.sort_unstable();
+    v.push(v[0] + 24 * 60);
+    v.windows(2).map(|w| w[1] - w[0]).min().unwrap()
 }
 
 #[cfg(test)]
