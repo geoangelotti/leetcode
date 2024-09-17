ace20893efcd0c1c91ff6afd6302ae3edf41bd31
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-17 14:25:58 +0200

    Uncommon Words from Two Sentences solution

--- js/src/UncommonWordsfromTwoSentences.mjs
+++ js/src/UncommonWordsfromTwoSentences.mjs
@@ -3,8 +3,25 @@
  * @param {string} s2
  * @return {string[]}
  */
-var uncommonFromSentences = function(s1, s2) {
-        
+var uncommonFromSentences = function (s1, s2) {
+    const dictionary = new Map();
+    s1.split(" ").filter(w => w.length > 0).forEach(w => {
+        if (dictionary.has(w)) {
+            const c = dictionary.get(w);
+            dictionary.set(w, c + 1);
+        } else {
+            dictionary.set(w, 1);
+        }
+    });
+    s2.split(" ").filter(w => w.length > 0).forEach(w => {
+        if (dictionary.has(w)) {
+            const c = dictionary.get(w);
+            dictionary.set(w, c + 1);
+        } else {
+            dictionary.set(w, 1);
+        }
+    });
+    return Array.from(dictionary.entries()).filter(([_, v]) => v === 1).map(([k, _]) => k);
 };
 
 export default uncommonFromSentences;
