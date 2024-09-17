292cc8ccaccda5585ac606c3ab3b416076350178
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-17 21:59:12 +0200

    Reverse Bits solution

--- go/solutions/reverse_bits.go
+++ go/solutions/reverse_bits.go
@@ -1,5 +1,11 @@
 package solutions
 
 func ReverseBits(num uint32) uint32 {
-	panic("Not implementeds")
+	var res uint32
+	for i := 0; i < 32; i++ {
+		res = res << 1
+		res = res | (num & 1)
+		num = num >> 1
+	}
+	return res
 }
