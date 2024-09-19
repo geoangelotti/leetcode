422dc8e47da67eac7588227f7d74f94a7fb4b5f1
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-19 18:10:40 +0200

    Largest Number solution

--- js/src/LargestNumber.mjs
+++ js/src/LargestNumber.mjs
@@ -3,7 +3,12 @@
  * @return {string}
  */
 var largestNumber = function(nums) {
-    
+	const array = nums.map(String);
+    array.sort((a, b) => (b + a).localeCompare(a + b));
+    if (array[0] === "0") {
+        return "0";
+    }
+    return array.join('');
 };
 
 export default largestNumber;
