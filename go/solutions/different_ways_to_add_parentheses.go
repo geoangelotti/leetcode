9a4ba5e10a237a001cc2a9006ad9fde42b78508d
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-19 18:25:59 +0200

    Different Ways to Add Parentheses solution

--- go/solutions/different_ways_to_add_parentheses.go
+++ go/solutions/different_ways_to_add_parentheses.go
@@ -1,5 +1,30 @@
 package solutions
 
+import "strconv"
+
 func DiffWaysToCompute(expression string) []int {
-	panic("Not implemented")
+	var res []int
+	for i := 0; i < len(expression); i++ {
+		operation := expression[i]
+		if operation == '+' || operation == '-' || operation == '*' {
+			s1 := DiffWaysToCompute(expression[:i])
+			s2 := DiffWaysToCompute(expression[i+1:])
+			for _, a := range s1 {
+				for _, b := range s2 {
+					if operation == '+' {
+						res = append(res, a+b)
+					} else if operation == '-' {
+						res = append(res, a-b)
+					} else if operation == '*' {
+						res = append(res, a*b)
+					}
+				}
+			}
+		}
+	}
+	if len(res) == 0 {
+		num, _ := strconv.Atoi(expression)
+		res = append(res, num)
+	}
+	return res
 }
