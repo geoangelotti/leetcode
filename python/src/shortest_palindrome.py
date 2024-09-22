0a55f8d22aa3c42001e05f3c4b5832128113818f
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-22 22:37:17 +0300

    Shortest Palindrome solution

--- python/src/shortest_palindrome.py
+++ python/src/shortest_palindrome.py
@@ -1,2 +1,17 @@
 def shortestPalindrome(s: str) -> str:
-    pass
+    prefix = 0
+    suffix = 0
+    base = 29
+    last_index = 0
+    power = 1
+    mod = 10**9+7
+    for i, c in enumerate(s):
+        char = (ord(c) - ord('a') + 1)
+        prefix = (prefix*base) % mod
+        prefix = (prefix+char) % mod
+        suffix = (suffix + char * power)
+        power = (power*base) % mod
+        if prefix == suffix:
+            last_index = i
+    suffix = s[last_index+1:]
+    return suffix[::-1] + s
