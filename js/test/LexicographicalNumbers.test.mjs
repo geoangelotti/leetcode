c64eafbd5a12881ea9929612b8c2e9121def8b08
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-09-22 12:46:44 +0300

    Lexicographical Numbers test

--- js/test/LexicographicalNumbers.test.mjs
+++ js/test/LexicographicalNumbers.test.mjs
@@ -3,14 +3,14 @@ import { strict as assert } from "node:assert";
 import lexicalOrder from "../src/LexicographicalNumbers.mjs";
 
 test("Return all the numbers in the range [1, n] sorted in lexicographical order", async (t) => {
-  await t.test("One", () => {
+  await t.test("Case 1", () => {
     assert.deepEqual(
       lexicalOrder(13),
       [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]
     );
   });
 
-  await t.test("Three", () => {
+  await t.test("Case 2", () => {
     assert.deepEqual(lexicalOrder(2), [1, 2]);
   });
 });
