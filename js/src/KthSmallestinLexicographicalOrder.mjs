b2a995f7512c14fb4ac8529eea0ff7b97b389d42
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-10-17 20:28:28 +0200

    K-th Smallest in Lexicographical Order solution

--- js/src/KthSmallestinLexicographicalOrder.mjs
+++ js/src/KthSmallestinLexicographicalOrder.mjs
@@ -1,12 +1,31 @@
-import lexicalOrder from "./LexicographicalNumbers.mjs";
 /**
  * @param {number} n
  * @param {number} k
  * @return {number}
  */
 var findKthNumber = function (n, k) {
-  const lexical = lexicalOrder(n);
-  return lexical[k - 1];
+  const getReqNum = (a, b, n) => {
+    let gap = 0;
+    while (a <= n) {
+      gap += Math.min(n + 1, b) - a;
+      a *= 10;
+      b *= 10;
+    }
+    return gap;
+  };
+  let num = 1;
+  let i = 1;
+  while (i < k) {
+    let req = getReqNum(num, num + 1, n);
+    if (i + req <= k) {
+      i += req;
+      num++;
+    } else {
+      i++;
+      num *= 10;
+    }
+  }
+  return num;
 };
 
 export default findKthNumber;
