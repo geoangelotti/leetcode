c0aecac0059700b41456b74a77f7ae2b3d2cd4a6
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-10-17 20:24:05 +0200

    K-th Smallest in Lexicographical Order slow solution

--- js/src/KthSmallestinLexicographicalOrder.mjs
+++ js/src/KthSmallestinLexicographicalOrder.mjs
@@ -1,8 +1,12 @@
+import lexicalOrder from "./LexicographicalNumbers.mjs";
 /**
  * @param {number} n
  * @param {number} k
  * @return {number}
  */
-var findKthNumber = function (n, k) {};
+var findKthNumber = function (n, k) {
+  const lexical = lexicalOrder(n);
+  return lexical[k - 1];
+};
 
 export default findKthNumber;
