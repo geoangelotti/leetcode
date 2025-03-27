195823b401bc934551a2dd255270e7c0bfb028cd
Geo Angelopoulos <{ID}+{username}@users.noreply.github.com>
2025-03-27 13:30:28 +0000

    Rotate String solution

--- python/src/rotate_string.py
+++ python/src/rotate_string.py
@@ -1,2 +1,9 @@
 def rotateString(s: str, goal: str) -> bool:
-    pass
+    if s == goal:
+        return True
+    temp = s[1:] + s[0]
+    while temp != s:
+        if temp == goal:
+            return True
+        temp = temp[1:] + temp[0]
+    return False
