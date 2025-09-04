ff6f2e44033fb6b06fae50b072183653f0f30e77
Georgios Angelopoulos <geoangelotti@users.noreply.github.com>
2025-09-04 19:50:16 +0300

    Find Closest Person solution

--- python/src/find_closest_person.py
+++ python/src/find_closest_person.py
@@ -1,2 +1,8 @@
 def findClosest(x: int, y: int, z: int) -> int:
-    pass
+    match (abs(x - z), abs(y - z)):
+        case (a, b) if a == b:
+            return 0
+        case (a, b) if a > b:
+            return 2
+        case _:
+            return 1
